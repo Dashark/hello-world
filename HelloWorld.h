6dfdefc8aaa0cff57218f92f993623c872fa9c05
ZFancy <32093003+ZFancy@users.noreply.github.com>
2018-03-08 21:48:54 +0800

    HelloWorld.cpp

--- HelloWorld.h
+++ HelloWorld.h
@@ -1,6 +1,13 @@
 #ifndef _HELLO_WORLD_
 #define _HELLO_WORLD_
+#include<iostream>
+using namespace std;
+
 class HelloWorld {
-  
+	public:
+		void print()
+		{
+			cout<<"HelloWorld"<<"\n"<<endl;
+		}
 };
 #endif
