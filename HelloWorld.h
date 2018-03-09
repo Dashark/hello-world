93047d3936cb9a7e6e7b3cd919f9b5570812fe79
uuuser123 <37216153+uuuser123@users.noreply.github.com>
2018-03-09 21:43:51 +0800

    Update HelloWorld.h

--- HelloWorld.h
+++ HelloWorld.h
@@ -1,6 +1,13 @@
 #ifndef _HELLO_WORLD_
 #define _HELLO_WORLD_
+
+#include<iostream>
+using namespace std;
 class HelloWorld {
-  
+public:
+int print_helloworld(){
+cout<<"Hello World!"<<endl;
+return 0;
+}
 };
 #endif
