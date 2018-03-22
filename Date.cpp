6d4421940b7cfeb6f45dfa1bad32ef72c9b90b0c
LeeTreeMoon <37114694+LeeTreeMoon@users.noreply.github.com>
2018-03-22 08:25:34 +0800

    Date.cpp
    类

--- Date.cpp
+++ Date.cpp
@@ -1,7 +1,11 @@
+#include<iostream> 
+using namespace std;
+
+int a[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
 class Date {
 public:
   Date(int y, int m, int d);
-  ~Date();
+//  ~Date();
 
   void nextDay();
   void previousDay();
@@ -12,6 +16,68 @@ private:
   int month;
   int day;
 };
+
+Date::Date(int y,int m,int d)
+{
+	year=y;
+	month=m;
+	day=d;
+}
+
+void Date::nextDay()
+{
+	if(year%400==0||year%100!=0&&year%4==0)
+	a[2]=29;
+	else
+	a[2]=28;	
+	if(day<a[month])
+	day++;
+	else if(day==a[month]&&month==12)
+	{
+		day=1;
+	    month=1;
+	    year++;
+	}
+	else
+	{
+		day=1;
+		month++;
+	}
+}
+
+void Date::previousDay()
+{
+	if(year%400==0||year%100!=0&&year%4==0)
+	a[2]=29;
+	else
+	a[2]=28;
+	if(day>1)
+	day--;
+	else if(day==1&&month==1)
+	{
+		month=12;
+		day=31;
+		year--;
+	}
+	else
+	{
+		month--;
+		day=a[month];
+	}
+}
+
+void Date::show()
+{
+	cout<<year<<"Äê"<<month<<"ÔÂ" <<day<<"ÈÕ"<<endl;
+}
+
+bool Date::equal(Date other)
+{
+	if(other.day==day&&other.month==month&&other.year)
+	return true;
+	else
+	return false;
+}
 
 int main() {
   Date today(2018,3,20);
