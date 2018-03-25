8a5a85961f736ecc136b89614fbad64a9693599e
HugoAhoy <zhjd3302@outlook.com>
2018-03-25 20:38:46 +0800

    Date Assignments

--- Date.c
+++ Date.c
@@ -1,27 +1,91 @@
+#include<stdio.h>
 struct Date {
-  int year;
-  int month;
-  int day;
+    int year;
+    int month;
+    int day;
 };
-void next_day(struct Date day);
-void previous_day(struct Date day);
+void next_day(struct Date* day);
+void previous_day(struct Date* day);
 int equal(struct Date one, struct Date day);
 void show(struct Date day) {
-  printf();
+    printf("The day is %d.%d.%d\n",day.year,day.month,day.day);
+      return;
 }
+
 int main() {
-  struct Date today = {2018,3,20};
-  struct Date day = {2018,3,20};
-  show(day);
-  for(int i=0;i<30;i++)
-    next_day(day);
-  show(day);
-  for(int i=0;i<30;i++)
-    previous_day(day);
-  show(day);
-  if(equal(today, day))
-    printf("Very Good!\n");
-  else
-    printf("Very Bad!\n");
-  return 0;
+    int i;
+    struct Date today = {2018,3,20};
+    struct Date day = {2018,3,20};
+    show(day);
+    for(i=0;i<30;i++)
+        next_day(&day);
+      show(day);
+      for(i=0;i<30;i++)
+        previous_day(&day);
+      show(day);
+      if(equal(today, day))
+        printf("Very Good!\n");
+      else
+        printf("Very Bad!\n");
+      return 0;
+}
+
+void next_day(struct Date* day){
+      int d;
+      int Feb = 28;
+      if((day->year%400 == 0)||((day->year%100 != 0)&&day->year%4 == 0)){
+        Feb = 29;
+      }
+      switch(day->month){
+        case 1: case 3: case 5:
+        case 7: case 8: case 10:
+        case 12:
+        d = 31; break;
+    
+        case 4: case 6: case 9: case 11:
+        d = 30; break;
+        case 2: d = Feb;
+      }
+      if ((day->day = day->day + 1) >d){
+           day->month += 1;
+        day->day = 1;
+    }
+    if (day->month == 13){
+        day->year += 1;
+        day->month = 1;
+    }
+    return;
+}
+
+void previous_day(struct Date* day){
+      int Feb = 28;
+      if((day->year%400 == 0)||((day->year%100 != 0)&&day->year%4 == 0)){
+        Feb = 29;
+      }
+      if ((day->day = day->day - 1) == 0)
+      {
+        day->month -= 1;
+        if (day->month == 0){
+        day->month = 12;
+        day->year -= 1;
+          }
+        switch(day->month){
+            case 1: case 3: case 5:
+            case 7: case 8: case 10:
+            case 12:
+            day->day = 31; break;
+    
+            case 4: case 6: case 9: case 11:
+            day->day = 30; break;
+            case 2: day->day = Feb;
+          }
+    }
+      return;
+}
+
+int equal(struct Date a,struct Date b){
+      if ((a.day == b.day)&&(a.year == b.year)&&(a.month == b.month)){
+        return 1;
+    }
+      else return 0;
 }
--- Date.cpp
+++ Date.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+using namespace std;
+
 class Date {
 public:
   Date(int y, int m, int d);
@@ -13,6 +16,73 @@ private:
   int day;
 };
 
+Date::Date(int y, int m, int d):year(y),month(m),day(d){}
+
+Date::~Date(){}
+
+void Date::nextDay(){
+	int d;
+	int Feb = 28;
+	if((year%400 == 0)||((year%100 != 0)&&year%4 == 0)){
+	Feb = 29;
+  }
+  switch(month){
+		case 1: case 3: case 5:
+    case 7: case 8: case 10:
+    case 12:
+    d = 31; break;
+    
+    case 4: case 6: case 9: case 11:
+    d = 30; break;
+    case 2: d = Feb;
+  }
+  if ((day = day + 1) >d){
+	 	month += 1;
+		day = 1;
+	}
+	if (month == 13){
+		year += 1;
+		month = 1;
+	}
+  return;
+}
+
+void Date::previousDay(){
+	int Feb = 28;
+	if((year%400 == 0)||((year%100 != 0)&&year%4 == 0)){
+  	Feb = 29;
+	}
+	if ((day = day - 1) == 0)
+	{
+  	month -= 1;
+  	if (month == 0){
+   	month = 12;
+  	year -= 1;
+	} 
+	  switch(month){
+		  case 1: case 3: case 5:
+		  case 7: case 8: case 10:
+		  case 12:
+ 		  day = 31; break;
+
+  	  case 4: case 6: case 9: case 11:
+		  day = 30; break;
+		  case 2: day = Feb;
+    }
+  } 
+  return;
+}
+
+bool Date::equal(Date other){
+  if((this->day == other.day)&&(this->month == other.month)&&(this->year == other.year)){
+    return true;
+  }
+  else return false;
+}
+
+void Date::show(){
+  cout<<"The date is "<<year<<"."<<month<<"."<<day<<endl;
+}
 int main() {
   Date today(2018,3,20);
   Date day(2018,3,20);
@@ -20,7 +90,7 @@ int main() {
   for(int i=0;i<30;i++)
     day.nextDay();
   day.show();
-  for(int i=0;i<30;i++)
+  for(i=0;i<30;i++)
     day.previousDay();
   day.show();
   if(day.equal(today))
