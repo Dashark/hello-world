0045b61daae52e17f012e54caf8342108c452c55
FingerBlack <37107553+FingerBlack@users.noreply.github.com>
2018-03-26 23:25:02 +0800

    Update Date.c

--- Date.c
+++ Date.c
@@ -3,11 +3,28 @@ struct Date {
   int month;
   int day;
 };
+int month1[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+int month2[12]={31,29,31,30,31,30,31,31,30,31,30,31};
 void next_day(struct Date day);
 void previous_day(struct Date day);
 int equal(struct Date one, struct Date day);
+bool 
 void show(struct Date day) {
   printf();
+}
+void next_day(struct Date day){
+  if(isrunyue){
+  }
+  if(day.day+1>)
+}
+void previous_day(struct Date day){
+  day.
+}
+int equal(struct Date one, struct Date day){
+  if()
+}
+void show(struct Date day) {
+  printf("%d %d %d\n",day.year,day.month,day.day);
 }
 int main() {
   struct Date today = {2018,3,20};
