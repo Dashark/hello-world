1445796bb997807d4bf86678553533a1c6372612
FingerBlack <37107553+FingerBlack@users.noreply.github.com>
2018-03-27 11:56:52 +0800

    Update Prime.cpp

--- Prime.cpp
+++ Prime.cpp
@@ -11,7 +11,47 @@ public:
 private:
   const int mNum;
 };
+Prime::Prime(int num){
+  mNUM=num;
+}
+Prime Prime::sumbit() {
+  int a=mNum,sum;
+  while(a){
+    sum+=num%10;
+    a/=10;
+  }
+  Prime b(sum);
+  return b;
+}
 
+Prime Prime::multibit() {
+ int a=mNum,sum;
+  while(a){
+    sum*=num%10;
+    a/=10;
+  }
+  Prime b(sum);
+  return b;
+}
+
+Prime Prime::squaresumbit(Prime num) {
+ int a=mNum,sum;
+  while(a){
+    sum+=(num%10)*(num%10);
+    a/=10;
+  }
+  Prime b(sum);
+  return b;
+}
+
+bool isprime(int num) {
+  for(int i=2;i*i<=num;i++){
+    if(num%i==0){
+      return false;
+    }
+  }
+  return true;
+}
 int main() {
   Prime pm(113);
   Prime sb = pm.sumBit();
