515e164c80e612638e6b1d4b46463dee5528f146
FingerBlack <37107553+FingerBlack@users.noreply.github.com>
2018-03-27 19:37:58 +0800

    Update Prime.c

--- Prime.c
+++ Prime.c
@@ -1,54 +1,55 @@
 /*满足下列条件的自然数称为超级素数:该数本身,所有数字之和,所有数字之积以及所有数字的平方和都是素数.例如113就是一个超级素数.求[100,9999]之内:(1)超级素数的个数.(2)所有超级素数之和.(3)最大的超级素数.*/
 #include<iostream>
 using namespace std;
+bool isprime(int num) {
+	for (int i = 2; i*i <= num; i++){
+		if (num%i == 0){
+			return false;
+		}
+	}
+	return true;
+}
 int sum_bit(int num) {
-  int sum;
-  while(sum){
-    sum+=num%10;
-    num/=10;
-  }
-  if(isprime(num)){
-    return true;
-  }
-  return 0;
+	int sum = 0;
+	while (num){
+		sum += num % 10;
+		num /= 10;
+	}
+	if (isprime(num)){
+		return true;
+	}
+	return 0;
 }
 
 int multi_bit(int num) {
-  int sum;
-  while(sum){
-    sum*=num%10;
-    num/=10;
-  }
-  if(isprime(num)){
-    return true;
-  } 
-  return 0;
+	int sum = 0;
+	while (num){
+		sum *= num % 10;
+		num /= 10;
+	}
+	if (isprime(num)){
+		return true;
+	}
+	return 0;
 }
 
 int square_sum_bit(int num) {
-  int sum;
-  while(sum){
-    sum+=num%10*num%10;
-    num/=10;
-  }
-  if(isprime(num)){
-    return true;
-  }
-  return 0;
+	int sum = 0;
+	while (num){
+		sum += num % 10 * num % 10;
+		num /= 10;
+	}
+	if (isprime(num)){
+		return true;
+	}
+	return 0;
 }
 
-bool isprime(int num) {
-  for(int i=2;i*i<=num;i++){
-    if(num%i==0){
-      return false;
-    }
-  }
-  return true;
-}
+
 
 int main() {
-  if(isprime(113)&&isprime(sum_bit(113))&&isprime(multi_bit(113))&&isprime(square_sum_bit(113))){
-    cout<<This is prim num<<endl;
-  }
-    //to do sth
+	if (isprime(113) && isprime(sum_bit(113)) && isprime(multi_bit(113)) && isprime(square_sum_bit(113))){
+		cout << "This is prim num " << endl;
+	}
+	//to do sth
 }
