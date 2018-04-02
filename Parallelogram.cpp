9d749e37bdbd2cea7674ebafd08c90ed7bc93b98
banye1998 <37111444+Liuxiaoyu1998@users.noreply.github.com>
2018-04-02 10:53:04 +0800

    Update Parallelogram.cpp
    cannot be profiled :(

--- Parallelogram.cpp
+++ Parallelogram.cpp
@@ -1,10 +1,47 @@
+#include <iostream>
+using namespace std;
+
 class Parallelogram {
+public:
+	Parallelogram(double l, double h);
+	
+	void show();
+	double area();
+	double height();
+	double length();
+private:
+	double height;
+	double length;
 };
-int main() {
-  Parallelogram pg;
-  pg.show();
-  pg.area();
-  pg.height();
-  pg.length();
-  return 0;
+
+Parallelogram::Parallelogram(double l, double h) {
+	height = h;
+	length = l;
+}
+
+void Parallelogram::show() {
+	cout << "the height is" << height << endl;
+	cout << "the length is" << length << endl;
+	cout << "the area is" << height * length << endl;
+}
+
+double Parallelogram::area() {
+	return height * length;
+}
+
+double Parallelogram::height() {
+	return height;
+}
+
+double Parallelogram::length() {
+	return length;
+}
+
+int main(void) {
+	Parallelogram pg(3.5, 4);
+	pg.show();
+	pg.area();
+	pg.height();
+	pg.length();
+	return 0;
 }
