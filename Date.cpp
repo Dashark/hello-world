9e51deec6e163198571c0f5af78512babee5e4fb
Auchinleck <37109098+Auchinleck@users.noreply.github.com>
2018-04-02 22:02:13 +0800

    Update Date.cpp

--- Date.cpp
+++ Date.cpp
@@ -1,31 +1,149 @@
-class Date {
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int a[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
+
+	class Date {
+
 public:
+
   Date(int y, int m, int d);
-  ~Date();
+
+
+
+
 
   void nextDay();
+
   void previousDay();
+
   bool equal(Date other);
+
   void show();
+
 private:
+
   int year;
+
   int month;
+
   int day;
+
 };
 
+void Date::Date(int y,int m,int d)
+{
+	year=y;
+	month=m;
+	day=d;
+}
+void Date::nextDay()
+{
+if(year%400==0||year%100!=0&&year%4==0) 
+{
+
+a[2]=29;
+}
+else 
+
+{
+a[2]=28;
+}
+if(day<a[month]) 
+{
+	day=day+1;
+} 
+else if(day==a[month]&&month==12) 
+{
+
+day=1; 
+month=1; 
+year=year+1;
+
+ } 
+else 
+
+{
+day=1; 
+month=month+1;
+ 
+}
+}
+void Date::previousDay()
+{
+if(year%400==0||year%100!=0&&year%4==0) 
+{
+
+a[2]=29;
+}
+else 
+
+{
+a[2]=28;
+}
+if(day>1) 
+{
+	day=day-1;
+} 
+else if(day==1&&month==1) 
+{
+
+day=31; 
+month=12; 
+year=year-1;
+
+ } 
+else 
+
+{
+day=a[month]; 
+month=month-1;
+ 
+}
+}
+void Date::show()
+{
+	cout<<year<<"."<<month<<"."<<day<<endl;
+}
+bool Date::equal(Date other)
+{
+	if(other.year==year&&other.month==month&&other.day==day)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
 int main() {
+
   Date today(2018,3,20);
+
   Date day(2018,3,20);
+
   day.show();
+
   for(int i=0;i<30;i++)
+
     day.nextDay();
+
   day.show();
+
   for(int i=0;i<30;i++)
+
     day.previousDay();
+
   day.show();
+
   if(day.equal(today))
+
     printf("Very Good!\n");
+
   else
+
     printf("Very Bad!\n");
+
   return 0;
+
 }
