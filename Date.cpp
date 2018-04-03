78d876e4e601509303b4b12ed185156952289f16
FingerBlack <37107553+FingerBlack@users.noreply.github.com>
2018-04-03 09:15:47 +0800

    Update Date.cpp

--- Date.cpp
+++ Date.cpp
@@ -1,3 +1,5 @@
+#include<iostream>
+#include<stdio.h>
 class Date {
 public:
   Date(int y, int m, int d);
@@ -12,6 +14,73 @@ private:
   int month;
   int day;
 };
+int month1[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
+int month2[13]={0,31,29,31,30,31,30,31,31,30,31,30,31};
+Date::Date(int y, int m, int d){
+	year=y;
+	month=m;
+	day=d;
+}
+Date::~Date(){
+}
+bool isrunnian(int year){
+  if(year%100==0){
+    if(year%4==0){
+        return true; 
+    }else{
+        return false;
+    }
+  }else{
+    if(year%4==0){
+        return true; 
+    }else{
+        return false;
+    }
+  }
+}
+void Date::nextDay(){
+	day++;
+    if(isrunnian(year)){
+      if(day>month2[month]){
+        day=1;
+        month++;
+      }
+    }else{
+      if(day>month2[month]){
+      day=1;
+        month++;
+    	}   
+    }
+	if(month>12){
+        month=1;
+        year++;
+    } 
+}
+void Date::previousDay(){
+	day--;
+    if(day==0){
+        month--;
+        if(isrunnian(year)){
+            day=month2[month];
+        }else{
+            day=month1[month];
+        }       
+    }
+  if(month==0){
+    month=12;
+    year--;
+  }
+}
+bool Date::equal(Date other){
+	if(other.year==year&&other.month==month&&other.day==day){
+        return true;
+    }else{
+        return false;
+    }
+}
+void Date::show() {
+  printf("%04d%01d%01d\n",year,month,day);
+}
 
 int main() {
   Date today(2018,3,20);
