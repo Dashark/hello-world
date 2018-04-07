b6c9162198d1f4350b1a57eb15ac06da4feaac5a
Ylllllllll <34728081+TheCleverYL@users.noreply.github.com>
2018-04-07 13:24:08 +0800

    Update Prime.cpp

--- Prime.cpp
+++ Prime.cpp
@@ -1,23 +1,62 @@
-class Prime {
+#include <iostream>
+using namespace std;
+
+class Prime{
 public:
-  Prime(int num);
-  ~Prime();
-
-  bool isPrime();
-
-  Prime sumBit();
-  Prime multiBit();
-  Prime sqaureSumBit();
+	Prime(int a){num =a;}
+	int getvalue(){return num;}
+	int pf(int);
+	int ji(int);
+	int he(int);
+	int is_prime(int);
 private:
-  const int mNum;
+	int num;
 };
 
-int main() {
-  Prime pm(113);
-  Prime sb = pm.sumBit();
-  Prime mb = pm.multiBit();
-  Prime ssb = pm.sqaureSumBit();
-  if(pm.isPrime() && sb.isPrime() && mb.isPrime() && ssb.isPrime())
-    ; //to do sth
-  return 0;
+int Prime::is_prime(int a){
+	if(a<=1) return 0;
+	if(a==2) return 1;
+	for(int i=2;i<a;i++){
+		if(a%i==0) return 0;
+	}
+	return 1;
+}
+int Prime::he(int a){
+	int total=0;
+	while(a!=0){
+		total+=a%10;
+		a/=10;
+	}
+	return total;
+}
+int Prime::ji(int a){
+	int total=1;
+	while(a!=0){
+		total*=a%10;
+		a/=10;
+	}
+	return total;
+}
+int Prime::pf(int a){
+	int total=0;
+	while(a!=0){
+		total+= (a%10)*(a%10);
+		a/=10;
+	}
+	return total;
+}
+
+
+int main(){
+	int ct=0,num=0,biggest=0;
+	for(int i=100;i<10000;i++){
+		Prime pm(i);
+		if(pm.is_prime(pm.getvalue() )  && pm.is_prime(pm.he(pm.getvalue()))&&  pm.is_prime(pm.ji(pm.getvalue())) &&pm.is_prime(pm.pf(pm.getvalue()))){
+			biggest=i;
+			ct+=i;
+			num++;
+		}
+	}
+	printf("%d %d %d",num,ct,biggest);
+	return 0;
 }
