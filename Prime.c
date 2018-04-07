0622da5a2a2ca8b363023e7af06bc24c31f1a782
Ylllllllll <34728081+TheCleverYL@users.noreply.github.com>
2018-04-07 13:11:55 +0800

    Update Prime.c

--- Prime.c
+++ Prime.c
@@ -1,21 +1,47 @@
-/*满足下列条件的自然数称为超级素数:该数本身,所有数字之和,所有数字之积以及所有数字的平方和都是素数.例如113就是一个超级素数.求[100,9999]之内:(1)超级素数的个数.(2)所有超级素数之和.(3)最大的超级素数.*/
-int sum_bit(int num) {
-  return 0;
+#include <stdio.h>
+using namespace std;
+int is_prime(int a){
+	if(a<=1) return 0;
+	if(a==2) return 1;
+	for(int i=2;i<a;i++){
+		if(a%i==0) return 0;
+	}
+	return 1;
 }
-
-int multi_bit(int num) {
-  return 0;
+int he(int a){
+	int total=0;
+	while(a!=0){
+		total+=a%10;
+		a/=10;
+	}
+	return total;
 }
-
-int square_sum_bit(int num) {
-  return 0;
+int ji(int a){
+	int total=1;
+	while(a!=0){
+		total*=a%10;
+		a/=10;
+	}
+	return total;
 }
-
-bool isprime(int num) {
-  return false;
+int pf(int a){
+	int total=0;
+	while(a!=0){
+		total+= (a%10)*(a%10);
+		a/=10;
+	}
+	return total;
 }
-
-int main() {
-  if(isprime(113)&&isprime(sum_bit(113))&&isprime(multi_bit(113))&&isprime(square_sum_bit(113)))
-    //to do sth
+int main(){
+	int ss[1000];int top=0;
+	for(int i=100;i<10000;i++){
+		if(is_prime(i) && is_prime(he(i)) && is_prime(ji(i)) && is_prime(pf(i)) ){
+			ss[top]=i;
+			top++;
+		}
+	}
+	int ct=0;
+	for(int j=0;j<top;j++) ct+=ss[j];
+	printf("%d %d %d",top,ct,ss[top-1]);
+	return 0;
 }
