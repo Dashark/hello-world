89b39644f0fd4bd0b37c61999616b7857c3c5e00
冯若航 <37108959+FeiBall@users.noreply.github.com>
2018-04-08 20:30:11 +0800

    Update Clock.cpp

--- Clock.cpp
+++ Clock.cpp
@@ -1,81 +1,70 @@
-//程序仍然有问题
-#include<iostream> 
+//程序仍有问题
+#include<iostream>
 using namespace std;
-
-class Clock 
-{
+class Clock {
 	public:
-		Clock()
-		{
-			hour=0;
-			minute=0;
-			second=0;
-		}
-//		~Clock();
-		
-		void adjust(int h,int m,int s);
-		void show();
-		void tick();
-	private:
-		int second,minute,hour;
+	void tick();
+    void show();
+    int adjust(int s,int m,int h);
+    
+    private:
+    	int sec;
+    	int min;
+    	int hour;
 };
 
-void Clock::adjust(int h,int m,int s)
-{
-	if(h>=0&&h<24&&m>=0&&m<60&&s>=0&&s<60)
+void Clock::tick(){
+	
+	
+	for(int i=0;i<=1000000;i++)
 	{
-		second=s;
-    	minute=m;
-    	hour=h;
-	}
-	else
-	cout<<"???????24???"<<endl; 
-}
-
-void Clock::show()
-{
-	if(hour<10) cout<<'0'<<hour;
-	else cout<<hour;
-	cout<<':';
-	if(minute<10) cout<<'0'<<minute;
-	else cout<<minute;
-	cout<<':';
-	if(second<10) cout<<'0'<<second;
-	else cout<<second;
-	cout<<endl;
-}
-
-void Clock::tick()
-{
-	//86400=24h
-	int i,j=0;
-	while(j<200)
-	{
-		second++;
-		if(second==60)
-		{ 
-		    second=0;
-			minute++;
+		sec++;
+		if(sec==60){
+		sec=0;
+		min++;
 		}
-		if(minute==60) 
-		{
-		    minute=0;
-		    hour++;
-	    } 
-	    if(hour==24)
-	    hour=0;
-	    for(i=0;i<=900000000;i++){};
-	    show();
-	    j++;
+	if(min==60){
+		min=0;
+		hour++;
+		}
+	if(hour==24){
+		hour=0;
+		
+		}
+		show();
 	}
 }
 
-int main()
+void Clock::show(){
+	if(hour<=9){cout<<"0"<<hour<<":";
+	}
+	else cout<<hour<<":";
+	if(min<=9){
+		cout<<"0"<<min<<":";
+	}
+	else cout<<min<<":";
+	if(sec<=9){
+		cout<<"0"<<sec;
+	}
+	else cout<<sec<<endl;
+}
+
+int Clock::adjust(int s,int m,int h)
 {
-    Clock ck;
-    ck.tick();
-    ck.show();
-    ck.adjust(16,45,00);
-    ck.show();
-    return 0;
+	if(0<=s&&s<=60&&m>=0&&m<=60&&h>=0&&h<=24){
+		sec=s;
+		min=m;
+		hour=h;
+	}
+	else cout<<"ÇëÊäÈë·ûºÏÕýÈ·Ê±¼ä¸ñÊ½µÄÊ±¼äµã"<<endl; 
+}
+
+
+int main() {
+  Clock ck;
+  ck.tick();
+  ck.show();
+  ck.adjust(16,45,00);
+  ck.show();
+  return 0;
 }
