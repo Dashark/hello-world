d7d736b5dbf3fff2fb281f326a047e96cded0748
banye1998 <37111444+Liuxiaoyu1998@users.noreply.github.com>
2018-04-16 10:49:58 +0800

    Update Clock.cpp
    show被包含在了tick里面；
    settime还不能中断tick；
    sleep（1000）是否会带来误差

--- Clock.cpp
+++ Clock.cpp
@@ -1,11 +1,62 @@
+#include <iostream>
+#include <iomanip>
+#include <windows.h>
+using namespace std;
 class Clock {
+	public:
+		Clock() {
+			this->hour = 0;
+			this->minute = 0;
+			this->second = 0;
+		}
+		~Clock() {}
+		
+		void tick() {
+			while(1) {
+				Sleep(1000);
+				second = (second+1) % 60;
+				if(!second) {
+					minute = (minute+1) % 60;
+					if(!minute) {
+						hour = (hour+1) % 24;
+					}
+				}
+				show();
+			}
+		}
+		void show() {
+			system("cls");
+			int curh = hour; int curm = minute; int curs = second;
+			cout << setw(2) << setfill('0') << curh << ": ";  
+			cout << setw(2) << setfill('0') << curm << ": ";  
+		    cout << setw(2) << setfill('0') << curs << endl; 
+		}
+		bool islegal(int h, int m, int s) {
+			if(h >= 0 && h <= 23 && m >= 0 && m <= 59 && s >= 0 && s <= 59) {
+				return true;
+			}
+			else return false;
+		} 
+		void adjust(int h, int m, int s) {
+			if(islegal(h, m, s) == false) {
+				cout << "Your setting is illegal." << endl;
+			}
+			else {
+				second = s;
+				minute = m;
+				hour = h;
+			}
+		}
+	
+	private:
+		int hour; int minute; int second;
 };
 
 int main() {
-  Clock ck;
-  ck.tick;
-  ck.show();
-  ck.adjust(16,45,00);
-  ck.show();
-  return 0;
+	Clock ck;
+	ck.tick();
+	ck.show();
+	ck.adjust(16,45,00); //判断输入是否合法 
+	ck.show();
+	return 0;
 }
