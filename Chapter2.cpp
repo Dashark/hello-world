12ae43909d9f62068d50f56a871b78ef25ab5aae
ZiyaoRen <2571476073@qq.com>
2018-04-27 21:21:28 +0800

    Finish Chapter2.cpp

--- Chapter2.cpp
+++ Chapter2.cpp
@@ -1,12 +1,55 @@
-#include <string>
-class Chapter2 {
-public:
-  Chapter2(){}
-  Chapter2(std::string &title, int pages):pages(pages),title(title) {}
-  void show() const {}
-private:
-  const string title; //章节标题固定的字符串
-  const int pages; //章节页数固定，不能改变
-  //复制章节对象没有意义，所以禁止
-  Chapter2(const Chapter2 &obj) {}
+#include<cstdlib>
+#include<cstdio>
+#include<cmath>
+#include<cstring>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<string>
+#include<iostream>
+
+using namespace std;
+
+class Chapter {
+	public:
+		Chapter():pages(0),title(""){}
+		Chapter(string &title, int pages):pages(pages),title(title) {}
+		void show() const {cout<<"title:"<<title<<" pages:"<<pages<<endl;}
+	private:
+		const string title; //章节标题固定的字符串
+		const int pages; //章节页数固定，不能改变
+		//复制章节对象没有意义，所以禁止
+		Chapter(const Chapter &obj):pages(0),title(""){}
+};
+
+class Book{
+	public:
+		Book(){}
+		
+		int insert(string &title,int pages)
+		{
+			book.push_back(new Chapter(title,pages));
+			return 0;
+		}
+		
+		int show()
+		{
+			for(int i=0;i<book.size();i++)
+				book[i]->show();
+			return 0;
+		}
+		
+	private:
+		const string bookname;
+		vector<Chapter *> book;
+};
+
+int main()
+{
+	string s="test";
+	Book b;
+	b.insert(s,3);
+	b.show();
+	return 0;
 }
+
