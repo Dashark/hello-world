41ccc6940a45eaeca09fac6bda3db85119b6258a
GitVide <37623201+GitVide@users.noreply.github.com>
2018-05-17 23:18:11 +0800

    定义函数

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -4,4 +4,8 @@
 void HelloWorld::hello() {
   std::cout << "Hello World!" << std::endl;
 }
-
+void HelloWorld::helloworld(){
+	std::cout << "Hello World!" << std::endl;
+}
+HelloWorld::helloworld(){
+}
