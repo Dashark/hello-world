363ca1bf95c35395b7e20384daeb23865fb096a5
GitHub <1162065497@qq.com>
2018-05-21 23:11:03 +0800

    Constructor a stock class

--- ClassConstructor.cpp
+++ ClassConstructor.cpp
@@ -1,2 +1,22 @@
-class {
+//stock00.h
+//version 00
+#ifndef STOCKOO_H_
+#define STOCKOO_H_
+#include<string>
+class Stock
+{private:
+	std::string company;
+	long shares;
+	double share_val;
+	double total_val;
+	void set_tot(){
+		total_val = shares * share_val;
+	}
+public:
+	Stock(const std::string & co,long n,double pr);
+	void buy(long num,double price);
+	void sell(long num,double price);
+	void update(double price);
+	void show();
 };
+#endif
