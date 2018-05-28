462a650a1b15c8a1ff2bcbeeef3415f833e0044f
Zfancy <814168136@qq.com>
2018-05-28 12:35:06 +0800

    new homework 2018/5

--- SuperPrime.cpp
+++ SuperPrime.cpp
@@ -1,3 +1,7 @@
+#include<iostream>
+#include<string>
+#include<math.h>
+using namespace std; 
 class Prime {
 public:
   Prime(int num);
@@ -19,11 +23,77 @@ public:
   Prime sqaureSumBit();
 private:
   const int num;
-}
+  
+};
 int main()
 {
     SuperPrime sp(113);
     if(sp.isPrime()) {
-        ; // do something
+    	//cout<<sp.isPrime()<<endl;
+        cout<<"do something//is SuperPrime"<<endl; // do something
     }
+    return 0;
+}
+Prime::Prime(int num):mNum(num){}
+
+bool Prime::isPrime(){
+	int flag=0;
+	for(int i=2;i<=sqrt(mNum);i++){
+		if(mNum%i==0){
+			flag=1;
+			break;
+		} 
+	}
+	if(flag==1){
+		
+		return false;
+	}
+	else{
+		return true;
+	}
+}
+SuperPrime::SuperPrime(int num):num(num),Prime(num){
+}
+bool SuperPrime::isPrime(){
+	if(sumBit().isPrime()&&multiBit().isPrime()&&sqaureSumBit().isPrime()){
+		return true;
+	}
+	else{
+		return false;
+		
+	}
+
+}
+SuperPrime::~SuperPrime(){
+
+}
+Prime::~Prime(){
+	
+}
+Prime SuperPrime::sumBit(){
+	int m=this->num;
+	int sum=0;
+	while(m!=0){
+		sum+=m%10;
+		m=m/10;
+	}
+	return Prime(sum); 
+}
+Prime SuperPrime::multiBit(){
+	int m=this->num;
+	int sum=1;
+	while(m!=0){
+		sum*=m%10;
+		m=m/10;
+	}
+	return Prime(sum); 
+}
+Prime SuperPrime::sqaureSumBit(){
+	int m=this->num;
+	int sum=0;
+	while(m!=0){
+		sum+=(m%10)*(m%10);
+		m=m/10;
+	}
+	return Prime(sum); 
 }
