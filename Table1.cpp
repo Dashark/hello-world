cac3a880aee5993c1722316605890542d01aa444
banye1998 <37111444+Liuxiaoyu1998@users.noreply.github.com>
2018-05-28 10:15:26 +0800

    Update Table1.cpp
    unable to use cell.

--- Table1.cpp
+++ Table1.cpp
@@ -1,23 +1,76 @@
+#include <iostream>
+#include <string.h>
 class Table {
-public:
-
-  void show();
-  int addRow();
-  void delRow(int n);
-  int addColumn();
-  void delColumn(int n);
-private:
-  Cell cells;
+	public:
+  	  Table() {
+	  	row = 20;
+	  	col = 20;
+	  	for(int i = 0; i < row; i++) {
+	  		for (int j = 0; j < col; j++) {
+	  		  atable[i][j] = '0';
+			}
+		}
+	  };
+	  Table(int row, int col) {
+	  	this->row = row;
+	  	this->col = col;
+	  	for(int i = 0; i < row; i++) {
+	  		for (int j = 0; j < col; j++) {
+	  		  atable[i][j] = '0';
+			}
+		}
+	  }
+	  ~Table() {};
+	  
+	  void show() {
+	  	for(int i = 0; i < 20; i++) {
+	  		for (int j = 0; j < 20; j++) {
+	  		  cout << atable[i][j] << " ";
+			}
+	  		cout << endl;
+		}
+	  };
+	  void addRow() {
+	  	row++;
+	  };
+	  void addColumn() {
+	  	col++;
+	  };
+	  void set(int row, int col, char data) {
+	  	atable[row-1][col-1] = data;
+	  };
+	  void delRow(int r) {
+	  	for(int i = r-1; i < row; i++) {
+	  		for (int j = 0; j < col; j++) {
+	  		  atable[i][j] = atable[i+1][j];
+			}
+		}
+	  };
+	  void delColumn(int c) {
+	  	for(int i = 0; i < row; i++) {
+	  		for (int j = c-1; j < col; j++) {
+	  		  atable[i][j] = atable[i][j+1];
+			}
+		}
+	  };
+	  
+	private:
+	  char atable[50][50];
+	  int row, col;
+  	  Cell cells;
 };
 class Cell {
 public:
-  Cell(char con[]);
+  Cell(char con[]) {
+  	con = "0";
+  };
   ~Cell();
-
   void show();
+  
 private:
   string content;
 }
+
 int main() {
   Table tb;
   tb.show();
@@ -25,6 +78,7 @@ int main() {
   tb.show();
   tb.addColumn();
   tb.show();
+  
   Table tb1(5,5);
   tb1.show();
   tb1.set(1,1,30);
