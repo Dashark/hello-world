b8724e933fca65c39ca5e808d1c3b93dbb0f2097
HugoAhoy <zhjd3302@outlook.com>
2018-06-09 09:29:12 +0800

    base Framework Units-0.0.0

--- Units.cpp
+++ Units.cpp
@@ -4,3 +4,24 @@
 //长度单位：m, km, ft, in,......
 //其它单位：md, cp,......
 //还有很多单位转换，有些单位是其它单位组合出来的
+#include<iostream>
+#include<string>
+#include<vector>
+
+class Unit{
+    public:
+      Unit(double num, std::string unit) : data(num), unit(unit){}
+      ~Unit(){}
+      virtual double convert(Unit &obj);
+
+    private:
+      double data;
+      std::string unit;
+};
+
+class Weight:virtual public Unit{
+    public:
+      Weight(double num, std::string unit) : Unit(num, unit){};
+    private:
+      static std::vector<std::vector<std::string> > Formula;
+};
