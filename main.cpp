2f9604afa41302c449b5aa882b66260f55374206
鱼尾 <36941866+wyw628729@users.noreply.github.com>
2018-06-12 09:29:40 +0000

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class hw{
+	public:
+		void hello();
+};
+
+void hw::hello(){
+	cout<<"helloworld"<<endl;
+}
+
+int main() { 
+   hw.hello(); 
+   return 0; 
+ } 
