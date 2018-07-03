7e49d20d65a1283434dcf2d334d8739d74a576b3
KingGilgamesh <37113685+KingGilgamesh@users.noreply.github.com>
2018-07-04 00:05:15 +0800

    Update Prime.cpp

--- Prime.cpp
+++ Prime.cpp
@@ -1,16 +1,68 @@
+#include <cstdio>  
+#include <cstring>  
+#include <algorithm>  
+#include <iostream>  
+#include <string>  
+#include <vector>  
+#include <stack>  
+#include <cmath>  
+
 class Prime {
 public:
-  Prime(int num);
-  ~Prime();
+  Prime(int num):mNum(num){ }
+  ~Prime(){ }
 
   bool isPrime();
 
   Prime sumBit();
   Prime multiBit();
   Prime sqaureSumBit();
+
 private:
   const int mNum;
 };
+
+bool Prime::isPrime() {
+  int tmp = (int)sqrt((double)Prime::mNum);
+  for (int i = 2; i <= tmp; ++i) {
+    if (Prime::mNum % i == 0)
+      return false;
+  }
+  return true;
+}
+
+Prime Prime::sumBit() {
+  int tmp = Prime::mNum;
+  int ans = 0;
+  while (tmp != 0) {
+    ans += (tmp % 10);
+    tmp /= 10;
+  }
+  Prime res(ans);
+  return res;
+}
+
+Prime Prime::multiBit() {
+  int tmp = Prime::mNum;
+  int ans = 1;
+  while (tmp != 0) {
+    ans *= (tmp % 10);
+    tmp /= 10;
+  }
+  Prime res(ans);
+  return res;
+}
+
+Prime Prime::sqaureSumBit() {
+  int tmp = Prime::mNum;
+  int ans = 0;
+  while (tmp != 0) {
+    ans += (tmp % 10) * (tmp % 10);
+    tmp /= 10;
+  }
+  Prime res(ans);
+  return res;
+}
 
 int main() {
   Prime pm(113);
@@ -18,6 +70,6 @@ int main() {
   Prime mb = pm.multiBit();
   Prime ssb = pm.sqaureSumBit();
   if(pm.isPrime() && sb.isPrime() && mb.isPrime() && ssb.isPrime())
-    ; //to do sth
+    std::cout << "Super Prime : 113" << std::endl; 
   return 0;
 }
