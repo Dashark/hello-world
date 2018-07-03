36b32165f929f69d4c4ce59a42f270420a733cad
ExecutantLK <37283634+ExecutantLK@users.noreply.github.com>
2018-07-04 01:19:22 +0800

    Update Date.cpp

--- Date.cpp
+++ Date.cpp
@@ -1,7 +1,10 @@
+#include<iostream>
+#include<math.h>
 class Date {
 public:
   Date(int y, int m, int d);
-  ~Date();
+  ~Date(){
+  };
 
   void nextDay();
   void previousDay();
@@ -12,6 +15,122 @@ private:
   int month;
   int day;
 };
+Date::Date(int y,int m,int d){
+	year=y;
+	day=d;
+	month=m;
+}
+void  Date::nextDay(){
+	if(month==12){
+		if(day!=31){
+			day++;
+		}
+		else{
+			day=1;
+			month=1;
+			year++;
+		}
+	}
+  else	if(month==1||month==3||month==5||month==7||month==8||month==10){
+		if(day!=31){
+			day++;
+		}
+		else{
+			day=1;
+		    month++;
+		}
+	}
+	else if(month==4||month==6||month==9||month==11){
+		if(day!=30){
+			day++;
+		}
+		else{
+		day=1;
+		    month++;
+		}
+	}
+	else if(month==2){
+		if((year%4==0&&year%100!=0)||year%400==0){
+	    	if(day!=29){
+			day++;
+	        	}
+	      	else{
+		day=1;
+		    month++;
+		}
+		}
+		else{
+		  if(day!=28){
+			day++;
+	        	}
+	      	else{
+		day=1;
+		   month++;
+		}
+			
+		}
+	}
+}
+void Date::previousDay(){
+		if(month==1){
+		if(day!=1){
+		day--;
+		}
+		else{
+		day=31;
+			month=12;
+		year--;
+		}
+	}
+	else if(month==12||month==5||month==7||month==8||month==10){
+		if(day!=1){
+			day--;
+		}
+		else{
+			day=30;
+		    month--;
+		}
+	}
+	else if(month==4||month==6||month==9||month==11||month==2){
+		if(day!=1){
+			day--;
+		}
+		else{
+			day=31;
+		    month--;
+		}
+	}
+   else 	if(month==3){
+		if((year%4==0&&year%100!=0)||year%400==0){
+	    	if(day!=1){
+			day--;
+	        	}
+	      	else{
+		day=29;
+		    month--;
+		}
+		}
+		else{
+		  if(day!=1){
+		day--;
+	        	}
+	      	else{
+			day=28;
+		   month--;
+		}
+			
+		}
+	}
+}
+void Date::show() {
+
+  printf("%d Äê %d ÔÂ %d ÈÕ\n",year,month,day);
+
+}
+bool Date::equal(Date other){
+	return false;
+}
+
 
 int main() {
   Date today(2018,3,20);
