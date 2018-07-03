3abadf21773de8d6f468688353e0bff13edf3704
ExecutantLK <37283634+ExecutantLK@users.noreply.github.com>
2018-07-04 01:03:11 +0800

    Update HelloWorld.h

--- HelloWorld.h
+++ HelloWorld.h
@@ -1,7 +1,17 @@
 #ifndef _HELLO_WORLD_
 #define _HELLO_WORLD_
+#include<iostream>
+using namespace std;
 class HelloWorld {
  public:
-  void hello();  
+  void hello(); 
 };
+HelloWorld::hello(){
+ cout<<"HelloWorld"<<endl;
+}
+int main(){
+HelloWorld hw;
+ hw.hello();
+ return 0;
+}
 #endif
