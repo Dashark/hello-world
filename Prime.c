39a93410ece7a6183b8e72acdbc5a315e38d1b90
ExecutantLK <37283634+ExecutantLK@users.noreply.github.com>
2018-07-04 00:54:35 +0800

    Update Prime.c

--- Prime.c
+++ Prime.c
@@ -1,31 +1,57 @@
 #include<stdio.h>
+#include<math.h>
 
 int sum_bit(int num) {
-
-  return 0;
+	int one=num%10;
+	int two=num/10%10;
+	int three=num/100%10;
+   int test;
+   test=one+two+three; 
+  return test;
 
 }
 
 
 
 int multi_bit(int num) {
-
-  return 0;
+	int one=num%10;
+	int two=num/10%10;
+	int three=num/100%10;
+   int test;
+ test=one*two*three; 
+  return test;
 
 }
 
 
 
 int square_sum_bit(int num) {
-
-  return 0;
-
+	int test;
+		int one=num%10;
+	
+		one*=one;
+	int two=num/10%10;
+	two*=two;
+	int three=num/100%10;
+	three*=three;
+   
+ test=one+two+three; 
+  return test;
 }
 
 
 
 bool isprime(int num) {
-
+	int flag=0;
+	for(int i=2;i<=sqrt(num);i++){
+		if(num%i==0){
+			flag=1;
+			break;
+		}
+	}
+	if(!flag){
+		return true;
+	}
   return false;
 
 }
@@ -35,13 +61,10 @@ bool isprime(int num) {
 int main() {
 	int n;
 	scanf("%d",&n);
-	int one=n%10;
-	int two=n/10%10;
-	int three=n/100%10;
-	printf("%d %d %d",one,two,three);
+  if(isprime(n)&&isprime(sum_bit(n))&&isprime(multi_bit(n))&&isprime(square_sum_bit(n)))
+  {
+  	printf("it's super primer\n");
+  }
 
-  //if(isprime(113)&&isprime(sum_bit(113))&&isprime(multi_bit(113))&&isprime(square_sum_bit(113)))
-
-    //to do sth
 return 0;
 }
