d87056315d8447f2d3c3257d0124717ef270edd7
GitHub <1162065497@qq.com>
2018-07-04 02:49:19 +0800

    s

--- Chapter2.cpp
+++ Chapter2.cpp
@@ -1,4 +1,4 @@
-#include <string>
+/*#include <string>
 class Chapter2 {
 public:
   Chapter2(){}
@@ -9,4 +9,33 @@ private:
   const int pages; //章节页数固定，不能改变
   //复制章节对象没有意义，所以禁止
   Chapter2(const Chapter2 &obj) {}
-}
+}*/
+#include<iostream>
+using namespace std;
+class Box
+{public:
+	Box(int,int,int);
+	int volume();
+private:
+	int height;
+	int width;
+	int length;
+ };
+ Box::Box(int h,int w,int len)
+ {
+ 	height=h;
+ 	width=w;
+ 	length=len;
+ }
+ int Box::volume()
+ {
+ 	return (height*width*length);
+ }
+ int main()
+ {
+ 	Box box1(12,12,12);
+ 	cout<<"The volume of box1 is"<<box1.volume()<<endl;
+ 	Box box2(13,13,13);
+ 	cout<<"The volume of box2 is"<<box2.volume()<<endl;
+ 	return 0;
+ }
