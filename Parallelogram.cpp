cbb8fefb5535acee9c97226c37947a092fbd0f8f
ExecutantLK <37283634+ExecutantLK@users.noreply.github.com>
2018-07-04 01:54:51 +0800

    Update Parallelogram.cpp

--- Parallelogram.cpp
+++ Parallelogram.cpp
@@ -1,5 +1,33 @@
+#include<iostream>
+using namespace std;
 class Parallelogram {
+	public:
+		Parallelogram(){
+			hei=1;
+			leng=1;
+		}
+		void show();
+		int area();
+		int height();
+		int length();
+	private:
+		int hei;
+		int leng;
+		
 };
+int  Parallelogram::height(){
+	return hei;
+}
+int Parallelogram::length(){
+	return leng;
+}
+int Parallelogram::area(){
+	return leng*hei;
+}
+void Parallelogram::show(){
+	cout<<leng*hei;
+}
+
 int main() {
   Parallelogram pg;
   pg.show();
