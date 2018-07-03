e31460664a7a0701baa89181ea617aeec5bb4214
GitHub <1162065497@qq.com>
2018-07-04 02:46:04 +0800

    s

--- Clock.cpp
+++ Clock.cpp
@@ -1,4 +1,4 @@
-class Clock {
+/*class Clock {
 };
 
 int main() {
@@ -8,4 +8,39 @@ int main() {
   ck.adjust(16,45,00);
   ck.show();
   return 0;
+}*/
+#include<iostream>
+using namespace std;
+class Time
+{
+	public:
+		Time()
+		{
+			hour=0;
+			minute=0;
+			sec=0;
+		}
+		void set_time();
+		void show_time();
+	private:
+		int hour;
+		int minute;
+		int sec;
+};
+void Time::set_time()
+{
+	cin>>hour;
+	cin>>minute;
+	cin>>sec;
+}
+void Time::show_time()
+{
+	cout<<hour<<':'<<minute<<':'<<sec<<endl;
+}
+int main()
+{
+	Time t1;
+	t1.set_time();
+	t1.show_time();
+	return 0;
 }
