495f57beaaffea6006db8c950c7362b542e21398
ExecutantLK <37283634+ExecutantLK@users.noreply.github.com>
2018-07-04 01:37:10 +0800

    Update ObjectLife.cpp

--- ObjectLife.cpp
+++ ObjectLife.cpp
@@ -1,4 +1,30 @@
-class {
-};
-int main() {
+#include<iostream> 
+#include<cctype>
+using namespace std;
+class date{
+	public:
+		void setdate(int y,int m,int d){
+			year=y;
+			month=m;
+			day=d;
+		}
+		int isleapyear(){
+			if((year%4==0&&year%100!=0)||(year%400==0)){
+				return 1;
+			}}
+		void print(){
+			cout <<year<<"年"<<month<<"月"<<day<<"日";
+		}
+	private:
+		int year;
+		int month;
+		int day;
+}; 
+
+int main(void){
+	date d1;
+	d1.setdate(2012,11,11);
+	d1.print();
+	cout<<"这一年是闰年吗"<<d1.isleapyear()<<endl;
+	return 0; 
 }
