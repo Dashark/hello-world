dc0bb5a9042a286e0dcec9197f9c7dcb5da9d6f5
ExecutantLK <37283634+ExecutantLK@users.noreply.github.com>
2018-07-04 01:48:27 +0800

    Update Clock.cpp

--- Clock.cpp
+++ Clock.cpp
@@ -1,11 +1,49 @@
+#include<iostream>
+using namespace std;
+
 class Clock {
+  public:
+	clock(){
+		hour=0;
+		minu=0;
+		s=0;
+	}
+	void adjust(int,int ,int);
+	void tick();
+	void show();
+ private:
+ 	int hour,minu,s;
+ 	
 };
+void Clock::adjust(int x,int y,int z){
+	hour=x;
+	minu=y;
+	s=z;
+	
+}
+void Clock::tick(){
+	cout<<"I don't know what is this"<<endl;
+	
+}
+void Clock::show(){
+	cout<<hour<<"Ð¡Ê±"<<minu<<"·ÖÖÓ"<<s<<"Ãë"<<endl;
+	
+}
+
+
 
 int main() {
+
   Clock ck;
-  ck.tick;
+
+  ck.tick();
+
   ck.show();
+
   ck.adjust(16,45,00);
+
   ck.show();
+
   return 0;
+
 }
