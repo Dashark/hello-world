70fdfe5693e122731f36e38709052055025eb73d
yuburgerG <37108491+yuburgerG@users.noreply.github.com>
2018-07-04 11:34:27 +0800

    Update Prime.cpp

--- Prime.cpp
+++ Prime.cpp
@@ -1,16 +1,61 @@
+#include<bits/stdc++.h>
 class Prime {
 public:
-  Prime(int num);
-  ~Prime();
+  Prime(int num):mNum(num){ }
+  ~Prime(){ }
 
   bool isPrime();
 
   Prime sumBit();
   Prime multiBit();
   Prime sqaureSumBit();
+
 private:
   const int mNum;
 };
+
+bool Prime::isPrime() {
+  int tmp = (int)sqrt((double)Prime::mNum);
+  for (int i = 2; i <= tmp; ++i) {
+    if (Prime::mNum % i == 0)
+      return false;
+  }
+  return true;
+}
+
+Prime Prime::sumBit() {
+  int tmp = Prime::mNum;
+  int ans = 0;
+  while (tmp != 0) {
+    ans += (tmp % 10);
+    tmp /= 10;
+  }
+  Prime res(ans);
+  return res;
+}
+
+Prime Prime::multiBit() {
+  int tmp = Prime::mNum;
+  int ans = 1;
+  while (tmp != 0) {
+    ans *= (tmp % 10);
+    tmp /= 10;
+  }
+  Prime res(ans);
+  return res;
+}
+
+Prime Prime::sqaureSumBit() {
+  int tmp = Prime::mNum;
+  int ans = 0;
+  while (tmp != 0) {
+    ans += (tmp % 10) * (tmp % 10);
+    tmp /= 10;
+  }
+  Prime res(ans);
+  return res;
+}
+
 
 int main() {
   Prime pm(113);
