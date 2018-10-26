eeaeed0c362dc33165e4bd0a945a09b0a5a57d52
HugoAhoy <zhjd3302@outlook.com>
2018-10-26 23:46:25 +0800

    first assignment

--- equation.c
+++ equation.c
@@ -2,3 +2,21 @@
 //ax + by = c
 //dx + ey = f
 //注意输入/输出，数据类型
+#include"stdio.h"
+
+int main() {
+    double a, b, c, d, e, f;
+    double ansx, ansy;
+    printf("Please enter two equation in the form of \'ax + by = c\'\n");
+    scanf("%lfx + %lfy = %lf",&a,&b,&c);
+    scanf("%lfx + %lfy = %lf",&d,&e,&f);
+    if(a * e == b * d || (a == 0&& b == 0) || (d == 0&&e == 0)) {
+        printf("It can't be solved(two equivalent equation)\n");
+    }
+    else {
+    ansx = (c * e -f * b)/(a * e - b * d);
+    ansy = (c * d - f * a)/(b * d - a * e);
+    printf("x = %lf\ny = %lf\n",ansx, ansy);
+    }
+    return 0;
+}
--- equation1.c
+++ equation1.c
@@ -1,3 +1,24 @@
 //求解一元二次方程的根
 //查阅sqrt()函数
 //注意有没有解
+#include"stdio.h"
+#include"math.h"
+
+int main() {
+    double a, b, c, d;
+    double delta;
+    printf("Please enter equation in the form of \'ax^2 + bx + c = d\'\n");
+    scanf("%lfx^2 + %lfx + %lf = %lf",&a,&b,&c,&d);
+    // printf("%lf\n%lf\n%lf\n%lf\n",a,b,c,d);
+    c = c - d;
+    delta = b * b - 4 * a * c;
+    // printf("%lf\n%lf\n%lf\n",a,b,c);
+    if(delta < 0) {
+        printf("No real-number solution\n");
+    }
+    else {
+        delta = sqrt(delta);
+        printf("x1 = %lf\nx2 = %lf\n",(-b - delta)/2/a,(-b + delta)/2/a);
+    }
+    return 0;
+}
