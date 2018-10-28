14a0e665940f3ac2bdefa460a68e742c49766233
zhuren2018 <43409863+zhuren2018@users.noreply.github.com>
2018-10-28 22:18:47 +0800

    第一次作业

--- equation.c
+++ equation.c
@@ -2,3 +2,33 @@
 //ax + by = c
 //dx + ey = f
 //注意输入/输出，数据类型
+#include<stdio.h>
+int main(void)
+{
+	float a, b, c, d, e, f;
+	while (scanf("%f%f%f%f%f%f", &a, &b, &c, &d, &e, &f) != EOF)
+	{
+		if (a*e - b * d == 0)
+		{
+			if (c*d - e * f != 0)
+			{
+				printf("y has no answer.\n");
+				if (c*e - b * f != 0)printf("x has no answer.\n");
+				else printf("x has infinite answers.\n");
+				continue;
+			}
+			if (c*e - b * f != 0)
+			{
+				printf("x has no answer.\n");
+				if (c*d - e * f != 0)printf("y has no answer.\n");
+				else printf("y has infinite answers.\n");
+				continue;
+			}
+			printf("x has infinite answers.\n");
+			printf("y has infinite answers.\n");
+			continue;
+		}
+		else printf("x=%.2f,y=%.2f", (c*e - b * f) / (a*e - b * d), (c*d - a * f) / (b*d - a * e));
+	}
+	return 0;
+}
--- equation1.c
+++ equation1.c
@@ -1,3 +1,28 @@
 //求解一元二次方程的根
 //查阅sqrt()函数
 //注意有没有解
+#include<stdio.h>
+#include<math.h>
+int main(void)
+{
+	float a, b, c;
+	while (scanf("%f%f%f", &a, &b, &c) != EOF)
+	{
+		if (a == 0) {
+			if (b == 0) {
+				if (c == 0)printf("There are infinite answers.\n");
+				else printf("There is no answer to x.\n");
+			}
+			else printf("x=%.2f", -c / b);
+
+		}
+		else {
+			if (b*b - 4 * a*c < 0)printf("There is no answer to x.\n");
+			else {
+				printf("x1=%.2f\n", (-b + sqrt(b*b - 4 * a*c)) / (2 * a));
+				printf("x2=%.2f\n", (-b - sqrt(b*b - 4 * a*c)) / (2 * a));
+			}
+		}
+	}
+	return 0;
+}
