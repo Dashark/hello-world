5c1e590ab4a2d9c5396e03c1863c8c1ce2e73314
zhiyunjiang981109 <1092403774@qq.com>
2018-10-29 12:37:38 +0800

    text.commit

--- equation.c
+++ equation.c
@@ -2,3 +2,13 @@
 //ax + by = c
 //dx + ey = f
 //注意输入/输出，数据类型
+#include <stdio.h>
+#include <math.h>
+int main(){
+	float a,b,c,d,e,f;
+	double x,y;
+	a*x+b*y == c;
+	d*x+e*y == f;
+	printf("%d\n",x,y);
+	return ;
+}
