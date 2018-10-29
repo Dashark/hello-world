4d9aed9af32aef81bd44b47c99328734bd155fe4
zhiyunjiang981109 <1092403774@qq.com>
2018-10-29 12:39:06 +0800

    test.commit

--- equation1.c
+++ equation1.c
@@ -1,3 +1,22 @@
 //求解一元二次方程的根
 //查阅sqrt()函数
 //注意有没有解
+#include <stdio.h>
+#include <math.h>
+void main(	float a,float b,float c) 
+{
+if(b*b-4*a*c>=0)
+{
+
+	double x,y;
+	x=(-b+sqrt(b*b-4*a*c))/(2*a);
+	y=(-b-sqrt(b*b-4*a*c))/(2*a);
+	printf("%f\n",x,y);
+	return ;	
+};
+	if (b*b-4*a*c<0)
+	{
+		printf("error\n");
+	};
+	return;
+}
