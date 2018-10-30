1aa2e33c630b50746c1743f425e3c73399ecafee
Nightfall1012 <752173494@qq.com>
2018-10-30 17:08:17 +0800

    equation add commit

--- equation.c
+++ equation.c
@@ -2,3 +2,19 @@
 //ax + by = c
 //dx + ey = f
 //æ³¨æ„è¾“å…¥/è¾“å‡ºï¼Œæ•°æ®ç±»å‹
+
+#include <stdio.h>
+int main (){
+	double a,b,c,d,e,f;
+	double x,y;
+	printf("ÇëÊäÈëÈı¸öÏµÊıa£¬b£¬c\n");
+	scanf ("%lf,%lf,%lf",&a,&b,&c);
+	printf("%lfx+%lfy=%lf\n",a,b,c);
+	printf("ÇëÊäÈëÈı¸öÏµÊıd£¬e£¬f\n");
+	scanf ("%lf,%lf,%lf",&d,&e,&f);	
+	printf("%lfx+%lfy=%lf\n",d,e,f);
+	x=(c*e-b*f)/(a*e-b*d);
+	y=(c*d-a*f)/(b*d-a*e);
+	printf("·½³Ì½âÎªx=%lf,y=%lf",x,y);
+	return 0;
+} 
--- equation1.c
+++ equation1.c
@@ -1,3 +1,38 @@
 //æ±‚è§£ä¸€å…ƒäºŒæ¬¡æ–¹ç¨‹çš„æ ¹
 //æŸ¥é˜…sqrt()å‡½æ•°
 //æ³¨æ„æœ‰æ²¡æœ‰è§£
+#include <stdio.h>
+#include <math.h>
+int main () {
+	double a,b,c;
+	double d;
+	double x,x1,x2; 
+	printf("ÇëÊäÈëÈı¸öÏµÊıa£¬b£¬c\n"); 
+	scanf("%lf,%lf,%lf",&a,&b,&c);
+	d=b*b-4*a*c;
+	if(a==0) {
+		if(b==0){
+			if(c==0){
+				printf("·½³ÌÎŞÒâÒå\n");							 
+			}
+			else{
+				printf("·½³ÌÎŞ½â\n"); 
+			}
+		}
+		else {
+			x=-c/b;
+			printf("·½³ÌÖ»ÓĞÒ»½âx=%lf\n",x); 
+		}
+	}
+	else {
+		if(d<0){
+			printf("·½³ÌÎŞÊµÊı½â\n");
+		}
+		else{
+			x1=(-b-sqrt(d))/(2*a);
+			x2=(-b+sqrt(d))/(2*a);
+			printf("·½³Ì½âÎªx1=%lf,x2=%lf",x1,x2);
+		}
+	}
+	return 0;
+}
