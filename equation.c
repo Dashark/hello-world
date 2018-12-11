feb778aa049f0bad09b3c897083104b700ac3985
waves-z <1751958394@qq.com>
2018-12-11 19:09:33 +0800

    equation add commit

--- equation.c
+++ equation.c
@@ -1,4 +1,9 @@
-//求解如下方程组
-//ax + by = c
-//dx + ey = f
-//注意输入/输出，数据类型
+#include <stdio.h>
+int main(){
+	double a,b,c,d,e,f,x,y;
+	scanf("%lf %lf %lf %lf %lf %lf",&a,&b,&c,&d,&e,&f);
+	y=(c*d-a*f)/(b*d-a*e);
+	x=(c-b*y)/a;
+	printf("%lf %lf",x,y);
+	return 0;
+}
