dbf25226758fb6527aeee5458fa0f789f16a350e
JiangHai0929 <1269998232@qq.com>
2018-12-24 20:12:25 +0800

    test commit

---
+++ SplitNum.c
@@ -0,0 +1,13 @@
+//输入整数，将每位数字打印出来（从左向右打印，空格区分）
+//2018/11/14 更新
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+int main() 
+{
+    //to do sth
+	int a;
+	scanf("%d", &a);
+	printf("%d", a);
+	return 0;
+}
