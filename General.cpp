02ab8d0cf1e30e3c41ed12982756750a60f15088
Lake Hu <Dashark@users.noreply.github.com>
2019-06-10 17:25:01 +0800

    static示例
    Singleton的设计模式

--- General.cpp
+++ General.cpp
@@ -1,19 +1,24 @@
 class Something : public Bigthing {
 	const Otherthing ot;
-public:
+	static int x;
 	Something() {
 	}
 	Something(int n):Bigthing(n), ot(n){
 	}
+public:
 	~Something(){
 	}
 	virtual void show() const {
 	}
 	virtual void show() {
 	}
+	static Something* create(int n) {
+		return new Something(n);
+	}
 };
 int main() {
-	Something st(123);
-	st.show();
+	Something *st = Something::create(123), *st1 = st->create(321);
+	st->show();
+	st1->show();
 	return 0;
 }
