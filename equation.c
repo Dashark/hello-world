660f89ba381e9d79a89d1dd5dbfff0726c73eee4
LuPengxuan <55118128+Aotoman01@users.noreply.github.com>
2019-10-22 08:15:50 +0800

    Add files via upload

--- equation.c
+++ equation.c
@@ -1,4 +1,17 @@
-//求解如下方程组
-//ax + by = c
-//dx + ey = f
-//注意输入/输出，数据类型
+#include <stdio.h>
+int main()
+{
+    float m = 1.78;
+    float x = 1.779999, y = 2;
+    float z = m / y, z1 = x / y;
+    float n = z - z1;
+	if(n > 0.00009999){
+    	printf("not equal");
+	}
+	else{
+		z = ( m + x ) / y;
+	}
+	printf("z is %.2f\n",z);
+	printf("z1 is %f\n",z1);
+	return 0;
+}
