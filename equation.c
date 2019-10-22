d9ef70c433a97d3903309ceca94f4a7addad101d
LuPengxuan <55118128+Aotoman01@users.noreply.github.com>
2019-10-22 09:19:49 +0800

    Add files via upload

--- equation.c
+++ equation.c
@@ -1,17 +1,15 @@
-#include <stdio.h>
-int main()
-{
-    float m = 1.78;
-    float x = 1.779999, y = 2;
-    float z = m / y, z1 = x / y;
-    float n = z - z1;
-	if(n > 0.00009999){
-    	printf("not equal");
+#include<stdio.h> 
+#include<math.h>
+
+int main(){
+	float m , n , z;
+	scanf("%f%f",&m,&n);
+	z = fabs(m - n);
+	if(z < 1e-5){
+		printf("equal");
+	} 
+	else {
+	printf("not equal");
 	}
-	else{
-		z = ( m + x ) / y;
-	}
-	printf("z is %.2f\n",z);
-	printf("z1 is %f\n",z1);
 	return 0;
 }
