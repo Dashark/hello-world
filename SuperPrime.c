0f993a314af7ea495866ca5dad39596631d2623d
qingfusheng <61900087+qingfusheng@users.noreply.github.com>
2020-03-21 20:21:31 +0800

    the superprime.c

--- SuperPrime.c
+++ SuperPrime.c
@@ -1,10 +1,60 @@
-//è¶…çº§ç´ æ•°ï¼šå®ƒæœ¬èº«ï¼Œå„ä½æ•°å­—çš„å’Œï¼Œå„ä½æ•°å­—çš„å¹³æ–¹å’Œï¼Œéƒ½æ˜¯ç´ æ•°ã€‚
-//æ±‚ 100~10000 å†…çš„æ‰€æœ‰è¶…çº§ç´ æ•°ï¼ŒåŠå®ƒä»¬çš„å¹³å‡æ•°ã€‚
+//³¬¼¶ËØÊı£ºËü±¾Éí£¬¸÷Î»Êı×ÖµÄºÍ£¬¸÷Î»Êı×ÖµÄÆ½·½ºÍ£¬¶¼ÊÇËØÊı¡£
+//Çó 100~10000 ÄÚµÄËùÓĞ³¬¼¶ËØÊı£¬¼°ËüÃÇµÄÆ½¾ùÊı¡£
 
+#include<stdio.h>
+#include<math.h>
+int k=0;
 int isPrime(int x);
-int splitNum(int x, int num[]);
-
+int splitNum(int n,int Num[]);
 int main()
 {
+    int i;
+    int s=0;
+    int Num[300];
+    for ( i= 100; i < 10000; i++)
+    {
+        splitNum(i,Num);
+    }
+    printf("ÒÔÏÂÎª100~10000Ö®¼äµÄ³¬¼¶ËØÊı:\n");
+    for (i = 0; i < k; i++){
+    	s+=Num[i];
+    	printf("%d\n",Num[i]);
+	}
+	printf("The average of the digit is %lf",(double)s/k);
     return 0;
 }
+int isPrime(int x)
+{
+        if (x == 2 || x == 3)
+            return 1;
+        if (x % 6 != 1 && x % 6 != 5)
+            return 0;
+        int tmp = sqrt(x);
+        int i;
+        for (i = 5; i <= tmp; i += 6)
+            if (x % i == 0 || x % (i + 2) == 0)
+                return 0;
+        return 1;
+}
+int splitNum(int n,int Num[]) {
+    int a = n;
+    int b = 0;
+    int c = 0;
+    int num = n;
+    while (num > 0) {
+        b += num % 10;
+        num /= 10;
+    }
+    num = n;
+    while (num > 0) {
+        c += (num % 10) * (num % 10);
+        num /= 10;
+    }
+    if(isPrime(a)&&isPrime(b)&&isPrime(c)){
+    	Num[k++]=n;
+    	return 1;
+    }else
+    	return 0;
+    	
+}
+
