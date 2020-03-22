b6c522d41d178a69034ac0583ade5ff7a8b7478a
ssgaryss <61570340+ssgaryss@users.noreply.github.com>
2020-03-22 12:08:53 +0800

    datetime.cpp

--- datetime.cpp
+++ datetime.cpp
@@ -3,6 +3,7 @@ class DateTime {
 private:
   int year, month, day;
   int hour, minute, second;
+  int lyear,lmonth,lday;
 public:
   DateTime();
   ~DateTime();
@@ -10,24 +11,27 @@ public:
   void showMoon();  //作业：将当前公历转换为农历显示出来 
 };
 int main() {
-  DateTime dt, dt1;
-  std::cout << &dt << std::endl;
-  std::cout << &dt1 << std::endl;
+  DateTime dt;
+
   dt.showTime();
-  dt1.showTime();
+  dt.showMoon();
   return 0;
 }
 DateTime::DateTime()
 {
-  year = 2020; month = 3; day = 20;
-  hour = 11; minute = 27; second = 55;
+	year = 2020; month = 3; day = 20;
+	hour = 11; minute = 27; second = 55;
 }
 DateTime::~DateTime() 
 {
-  std::cout << this << " Go die, Ha~Ha~" << std::endl;
+  std::cout << "公历已被转换为农历" << std::endl;
 }
 void DateTime::showTime()
 {
   printf("当前时间：%d/%d/%d %d:%d:%d\n", year, month, day, hour, minute, second);
 }
- 
+void DateTime::showMoon()
+{
+	lyear = 2020; lmonth = 2; lday = 27;
+ 	printf("当前时间：%d/%d/%d %d:%d:%d\n", lyear, lmonth, lday, hour, minute, second);
+} 
