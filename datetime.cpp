c0ad1451ddae2dd5a7e79228bed9737037737a94
muqingyou <53122181+muqingyou@users.noreply.github.com>
2020-03-26 14:30:48 +0800

    datetime commit

--- datetime.cpp
+++ datetime.cpp
@@ -1,33 +1,142 @@
-#include <iostream>
-class DateTime {
-private:
-  int year, month, day;
-  int hour, minute, second;
-public:
-  DateTime();
-  ~DateTime();
-  void showTime();
-  void showMoon();  //作业：将当前公历转换为农历显示出来 
-};
-int main() {
-  DateTime dt, dt1;
-  std::cout << &dt << std::endl;
-  std::cout << &dt1 << std::endl;
-  dt.showTime();
-  dt1.showTime();
-  return 0;
-}
-DateTime::DateTime()
+#include<stdio.h>
+int Sub(int mon,int dat);
+void Lunar(int sub);
+void Zhong(int num);
+int main()
 {
-  year = 2020; month = 3; day = 20;
-  hour = 11; minute = 27; second = 55;
+	printf("请输入2020年的公历，如“1月1日”：");
+	int mon,dat,sub;
+	scanf("%d月%d日",&mon,&dat);
+	sub=Sub(mon,dat);
+	Lunar(sub);
+	return 0;
 }
-DateTime::~DateTime() 
+int Sub(int mon,int dat)
 {
-  std::cout << this << " Go die, Ha~Ha~" << std::endl;
+	int sub=0;
+	switch(mon){
+		case 1:break;
+		case 2:sub+=31;
+		break;
+		case 3:sub+=60;
+		break;
+		case 4:sub+=91;
+		break;
+		case 5:sub+=121;
+		break;
+		case 6:sub+=152;
+		break;
+		case 7:sub+=182;
+		break;
+		case 8:sub+=213;
+		break;
+		case 9:sub+=244;
+		break;
+		case 10:sub+=274;
+		break;
+		case 11:sub+=305;
+		break;
+		case 12:sub+=336;
+		break;
+	}
+	sub+=dat;
+	return sub;
 }
-void DateTime::showTime()
+void Zhong(int num){
+	switch(num){
+		case 1:printf("一");
+		break;
+		case 2:printf("二");
+		break;
+		case 3:printf("三");
+		break;
+		case 4:printf("四");
+		break;
+		case 5:printf("五");
+		break;
+		case 6:printf("六");
+		break;
+		case 7:printf("七");
+		break;
+		case 8:printf("八");
+		break;
+		case 9:printf("九");
+		break;
+		case 10:printf("十");
+		break;
+	}
+}
+void Lunar(int sub)
 {
-  printf("当前时间：%d/%d/%d %d:%d:%d\n", year, month, day, hour, minute, second);
+	if(sub<25){
+		printf("腊月");
+		sub+=6;
+	}
+	else if(sub<54){
+		printf("一月");
+		sub-=24;
+	}
+	else if(sub<84){
+		printf("二月");
+		sub-=53;
+	}
+	else if(sub<114){
+		printf("三月");
+		sub-=83;
+	}
+	else if(sub<144){
+		printf("四月");
+		sub-=113;
+	}
+	else if(sub<173){
+		printf("闰四月");
+		sub-=143;
+	}
+	else if(sub<203){
+		printf("五月");
+		sub-=172;
+	}
+	else if(sub<232){
+		printf("六月");
+		sub-=202;
+	}
+	else if(sub<261){
+		printf("七月");
+		sub-=231;
+	}
+	else if(sub<291){
+		printf("八月");
+		sub-=260;
+	}
+	else if(sub<320){
+		printf("九月");
+		sub-=290;
+	}
+	else if(sub<350){
+		printf("十月");
+		sub-=319;
+	}
+	else {
+		printf("十一月");
+		sub-=349;
+	}
+	
+	if(sub<=10){
+		printf("初");
+		Zhong(sub); 
+	}
+	else if(sub<20){
+		printf("十");
+		sub=sub-10;
+		Zhong(sub);
+	}
+	else if(sub=20)
+		printf("二十");
+	else if(sub<30){
+		printf("廿");
+		sub=sub-20;
+		Zhong(sub); 
+	}
+	else printf("三十");
 }
- 
+
