4e70f57bfd55e090b883a645be5750531d41a139
Dashark <dasha.hu@gmail.com>
2020-03-27 10:54:55 +0800

    overload constructor

--- datetime.cpp
+++ datetime.cpp
@@ -5,14 +5,13 @@ private:
   int hour, minute, second;
 public:
   DateTime();
+  DateTime(int y, int m, int d, int hour, int minute, int second); 
   ~DateTime();
   void showTime();
   void showMoon();  //作业：将当前公历转换为农历显示出来 
 };
 int main() {
-  DateTime dt, dt1;
-  std::cout << &dt << std::endl;
-  std::cout << &dt1 << std::endl;
+  DateTime dt, dt1(2020, 3, 27, 10, 40, 55);
   dt.showTime();
   dt1.showTime();
   return 0;
@@ -22,9 +21,14 @@ DateTime::DateTime()
   year = 2020; month = 3; day = 20;
   hour = 11; minute = 27; second = 55;
 }
+DateTime::DateTime(int y, int m, int d, int hour, int minute, int second)
+{
+  year = y; month = m; day = d;
+  this->hour = hour; this->minute = minute; this->second = second;
+}
 DateTime::~DateTime() 
 {
-  std::cout << this << " Go die, Ha~Ha~" << std::endl;
+  std::cout << " Go die, Ha~Ha~" << std::endl;
 }
 void DateTime::showTime()
 {
