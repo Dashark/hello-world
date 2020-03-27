f347528c341fdaf0e249f108a5b6d9852a1270db
Dashark <dasha.hu@gmail.com>
2020-03-27 11:46:32 +0800

    copy constructor/object reference/object pointer

--- datetime.cpp
+++ datetime.cpp
@@ -6,14 +6,20 @@ private:
 public:
   DateTime();
   DateTime(int y, int m, int d, int hour, int minute, int second); 
+  DateTime(const DateTime &dt);
   ~DateTime();
   void showTime();
   void showMoon();  //作业：将当前公历转换为农历显示出来 
 };
 int main() {
   DateTime dt, dt1(2020, 3, 27, 10, 40, 55);
+  DateTime dt2(dt), &dt3 = dt;
+  DateTime *dt4 = &dt;
   dt.showTime();
   dt1.showTime();
+  dt2.showTime();
+  dt3.showTime();
+  dt4->showTime();
   return 0;
 }
 DateTime::DateTime()
@@ -25,6 +31,11 @@ DateTime::DateTime(int y, int m, int d, int hour, int minute, int second)
 {
   year = y; month = m; day = d;
   this->hour = hour; this->minute = minute; this->second = second;
+}
+DateTime::DateTime(const DateTime &dt)
+{
+  year = dt.year; month = dt.month; day = dt.day;
+  hour = dt.hour; minute = dt.minute; second = dt.second;
 }
 DateTime::~DateTime() 
 {
