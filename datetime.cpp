9b373b30fe08d567475092494526037f3bc6e225
Dashark <dasha.hu@gmail.com>
2020-03-27 12:14:09 +0800

    static decorate

--- datetime.cpp
+++ datetime.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 class DateTime {
 private:
+  static int sth;
   int year, month, day;
   int hour, minute, second;
 public:
@@ -8,9 +9,12 @@ public:
   DateTime(int y, int m, int d, int hour, int minute, int second); 
   DateTime(const DateTime &dt);
   ~DateTime();
-  void showTime();
+  static void showTime();
   void showMoon();  //作业：将当前公历转换为农历显示出来 
 };
+
+int DateTime::sth = 0;
+
 int main() {
   DateTime dt, dt1(2020, 3, 27, 10, 40, 55);
   DateTime dt2(dt), &dt3 = dt;
@@ -26,6 +30,7 @@ DateTime::DateTime()
 {
   year = 2020; month = 3; day = 20;
   hour = 11; minute = 27; second = 55;
+  sth = 11;
 }
 DateTime::DateTime(int y, int m, int d, int hour, int minute, int second)
 {
@@ -44,5 +49,6 @@ DateTime::~DateTime()
 void DateTime::showTime()
 {
   printf("当前时间：%d/%d/%d %d:%d:%d\n", year, month, day, hour, minute, second);
+  std::cout << sth << std::endl;
 }
  
