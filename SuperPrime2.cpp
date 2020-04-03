fb18678ad9f933bf233d2220b4c86b524a87c293
Dashark <dasha.hu@gmail.com>
2020-04-03 12:01:55 +0800

    第二阶段框架：SuperPrime类

--- SuperPrime2.cpp
+++ SuperPrime2.cpp
@@ -1,16 +1,65 @@
 #include <iostream>
+class SuperPrime {
+  public:
+  	SuperPrime():number(0) {
+	}
+  	~SuperPrime() {
+	}
+  	bool isSuperPrime() {
+  	  split();
+  	  int a = sum();
+	  int b = multi();
+	  int c = squareSum();
+	  if (isPrime(number) && isPrime(a) && isPrime(b) && isPrime(c))
+	    return true; 
+  	  return false;
+	}
+  private:
+  	const int number;
+  	int N[100], size;
+  	bool isPrime(int n) { 
+  	
+  	  return false;
+	}
+	void split() {
+	  // number split into N
+	}
+	int sum() {
+	  return 0;
+	}
+	int multi() {
+	  return 0;
+	}
+	int squareSum() {
+	  return 0;
+	}
+};
 class Set {
   public:
   	Set(int from, int to) {
+  	  size = 0;
 	}
   	~Set() {
 	}
   	int count() {
+  	  int count = 0;
+  	  for (int i = 0; i < size; i++)
+  	    if(set[i].isSuperPrime())
+  	      count += 1;
+	  return count; 
 	}
   	int sum() {
+  	  int sum = 0;
+  	  /*
+  	  for (int i = 0; i < size; i++)
+  	    if(set[i].isSuperPrime())
+  	      sum += set[i];
+  	      */ 
+	  return sum; 
 	}
   private:
-  	int set[1000];
+  	SuperPrime set[1000];
+  	int size;
 };
 int main() {
   Set ss(100, 999);
