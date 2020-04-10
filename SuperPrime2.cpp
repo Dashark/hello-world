353584819affd411c7a9d91a33bd54524f25b96e
muqingyou <53122181+muqingyou@users.noreply.github.com>
2020-04-10 12:09:23 +0800

    superprime2

--- SuperPrime2.cpp
+++ SuperPrime2.cpp
@@ -15,51 +15,77 @@ class SuperPrime {
 	    return true; 
   	  return false;
 	}
+	int number;
   private:
-  	const int number;
-  	int N[100], size;
+  	int N[100], size,i;
   	bool isPrime(int n) { 
-  	
-  	  return false;
+	  	int flag=1;
+		for(i=2;i<n;i++){
+			if(n%i==0){
+				flag=0;
+				break;	
+			}		
+		}
+		if(flag)
+		return 1;
+		else	
+  		return false;
 	}
 	void split() {
+		int n=0;
+		do{
+			N[n]=number%10;
+			number=number/10;
+			n++;
+		}while(0!=number);
 	  // number split into N
 	}
 	int sum() {
-	  return 0;
+		int sum=0;
+		for(i=0;i<4;i++)
+			sum+=*(N+i);
+	  return sum;
 	}
 	int multi() {
-	  return 0;
+		int mul=1;
+		for(i=0;i<4;i++)
+			mul*=N[i];	
+	  return mul;
 	}
 	int squareSum() {
-	  return 0;
+		int sqsum=0;
+		for(i=0;i<4;i++)
+			sqsum+=N[i]*N[i];
+	  return sqsum;
 	}
 };
 class Set {
   public:
   	Set(int from, int to) {
-  	  size = 0;
+  	  size = to-from;
+  	  for(int i = from; i < to; ++i)
+  	  set[i] = NULL;
 	}
   	~Set() {
+	  	for(int i = 0; i < size; ++i)
+	  	  delete set[i];
 	}
   	int count() {
   	  int count = 0;
   	  for (int i = 0; i < size; i++)
-  	    if(set[i].isSuperPrime())
+  	    if(set[i]->isSuperPrime())
   	      count += 1;
 	  return count; 
 	}
-  	int sum() {
+  int sum() {
   	  int sum = 0;
-  	  /*
   	  for (int i = 0; i < size; i++)
-  	    if(set[i].isSuperPrime())
-  	      sum += set[i];
-  	      */ 
+  	    if(set[i]->isSuperPrime())
+  	      sum += set[i]->number;
 	  return sum; 
 	}
   private:
-  	SuperPrime set[1000];
+  	SuperPrime *set[1000];
   	int size;
 };
 int main() {
