ccaf9189ceb0f40762ab47a4141433ebd9d43fd7
Dashark <dasha.hu@gmail.com>
2020-05-15 12:02:48 +0800

    虚函数的语义与作用

--- SuperPrime2.cpp
+++ SuperPrime2.cpp
@@ -8,8 +8,9 @@ class Prime {
 	}
 	~Prime() {
 	}
-  	bool isPrime() { 
+  	virtual bool isPrime() { 
   	  //2到number-1的因子 
+  	  std::cout << "Prime's isPrime() call" << std::endl;
   	  return false;
 	}
   private:
@@ -65,7 +66,8 @@ class SuperPrime : public Prime {
 	}
   	~SuperPrime() {
 	}
-  	bool isPrime() {   //类/对象的接口，更抽象说是外观 
+  	virtual bool isPrime() {   //类/对象的接口，更抽象说是外观 
+  	  std::cout << "SuperPrime's isPrime() call" << std::endl;
 	  if (Prime::isPrime() && pset.isAllPrime())
 	    return true; 
   	  return false;
@@ -83,7 +85,7 @@ class SuperPrime : public Prime {
 	}
 };
 int main() {
-  Prime p(13);
+  SuperPrime p(13);
   SuperPrime sp(113);
   PrimeSet set(2);
   set.add(&sp); 
