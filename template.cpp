82b9ec15f8d3f33b519354eb5a1c683672629939
Dashark <dasha.hu@gmail.com>
2020-05-29 11:45:00 +0800

    数据类型表达

--- template.cpp
+++ template.cpp
@@ -4,7 +4,7 @@ T Max(T x, T y) {
   return x > y ? x : y;
 }
 int main() {
-  std::cout << "最大数：" << Max(7.7, 8.8) << std::endl; 
+  std::cout << "最大数：" << Max(7.0, 8.8) << std::endl; 
   std::cout << "最大数：" << Max(9, 8) << std::endl; 
   return 0;
 } 
