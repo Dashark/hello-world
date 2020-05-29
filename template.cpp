2316e85afc44d76fd02e62994780e22aac8452c8
Dashark <dasha.hu@gmail.com>
2020-05-29 10:45:56 +0800

    宏定义

--- template.cpp
+++ template.cpp
@@ -1,15 +1,7 @@
 #include <iostream>
-float Max(float x, float y) {
-  return x > y ? x : y;
-}
-int Max(int x, int y) {
-  return x > y ? x : y;
-}
-double Max(double x, double y) {
-  return x > y ? x : y;
-}
+#define Max(x,y)  (x > y ? x : y)
 int main() {
-  std::cout << "最大数：" << Max(7.7, 8.8) << std::endl; 
+  std::cout << "最大数：" << Max(7 + 0.7, 8 + 0.8) << std::endl; 
   std::cout << "最大数：" << Max(9, 8) << std::endl; 
   return 0;
 } 
