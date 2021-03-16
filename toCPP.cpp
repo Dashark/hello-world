5e1980c18ac3f0e921431b7c094a54c38d4c3bc0
Lake Hoo <hudasha@scu.edu.cn>
2021-03-16 17:37:40 +0800

    从C到C++

---
+++ toCPP.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdio.h>
+struct MyStruct {
+	char hello[50];
+	char name[50];
+}; 
+int myfunction(struct MyStruct* myinfo) {
+	std::cout << myinfo->name << "   "<< myinfo->hello << std::endl;
+	printf("%s   %s\n", myinfo->name, myinfo->hello);
+	return 10;
+}
+int main()
+{
+	struct MyStruct myinfo = {
+    	"Hello World!",
+	    "DaShark"
+	};
+	// call my function
+	return myfunction(&myinfo);
+} 
