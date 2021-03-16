edb63ad35d8ffe0884493b72ac42964b7db59489
Lake Hoo <hudasha@scu.edu.cn>
2021-03-16 18:23:18 +0800

    完整的C++

--- toCPP.cpp
+++ toCPP.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include <stdio.h>
-struct MyStruct {
+#include <string.h>
+class MyStruct {
+public:
 	char hello[50];
 	char name[50];
+	MyStruct(char h[], char n[]) {
+		strcpy(hello, h);
+		strcpy(name, n);
+	}
 	int myfunction() {
 		std::cout << name << "   "<< hello << std::endl;
 		printf("%s   %s\n", name, hello);
@@ -11,10 +17,10 @@ struct MyStruct {
 }; 
 int main()
 {
-	struct MyStruct myobj = {
-    	"Hello World!",
-	    "DaShark"
-	};
-	// call my function
-	return myobj.myfunction();
+	MyStruct myobj("Hello World!", "DaShark");
+	MyStruct myobj1("HAHAHAHA!", "DaShark");
+	// call my object's method
+	myobj.myfunction();
+	myobj1.myfunction();
+	return 0;
 } 
