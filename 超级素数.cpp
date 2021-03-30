106cd807eb0ea20badf0c7d16692ad3c68390171
snyss <80738842+snyss@users.noreply.github.com>
2021-03-30 10:01:44 +0800

    Add files via upload

---
+++ 超级素数.cpp
@@ -0,0 +1,45 @@
+#include<stdio.h> 
+f(int n)
+{
+	int z=1;
+	int i;
+	for(i=2;i<=n-1;i++)
+	{
+		if(n%i==0)
+		{
+			z=0;
+			break;
+		}
+	}
+	return (z);
+}
+int main()
+{
+	int a,b,c,m;
+	int num,sum,max;
+	num=0,sum=0,max=0;
+	
+	for(m=100;m<=999;m++)
+	{
+		if(f(m)==1)
+		{
+			a=m/100;
+			b=m%10/10;
+			c=m%10;
+			int x,y,z;
+	        x=a+b+c;
+	        y=a*b*c;
+	        z=a*a+b*b+c*c;
+			if((m<1000)&&(f(x)==1)&&(f(y)==1)&&(f(z)==1))
+			{
+				num+=1;
+				sum+=m;
+				max=m;
+			}
+		}
+	}
+	printf("[100,999]之内超级素数的个数为%d\n",num);
+	printf("[100,999]之内所有超级素数之和为%d\n",sum);
+	printf("[100,999]之内最大的超级素数为%d\n",max);
+	return 0;
+ } 
