27fe771a121153b6e366598a575415e41aa1b59e
Summit3Zz <80748678+Summit3Zz@users.noreply.github.com>
2021-04-13 20:50:45 +0800

    Add files via upload

--- c++zero.cpp
+++ c++zero.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
-class Superprime{
-	public:
-		void shell(){
-			    for(a=100;a<=999;a++){
+int main()
+{
+	int a,b=2,c[3]={0},m[1001]={0},n[3]={0},i=0,j=0,sum[3]={0},p=0,max=0;
+    for(a=100;a<=999;a++){
     	b=2;
     	while(1){
     	    if(a%b==0){
@@ -16,7 +16,6 @@ class Superprime{
 	    }
 	}
 	for(a=100,b=2;a<=999;a++){
-		b=2;
 		c[0]=c[1]=c[2]=0;
 		if(m[a]!=0){
 			c[0]=(a%100)%10;//个位
@@ -67,9 +66,6 @@ class Superprime{
 		    	break;
 			}
 			}
-		if(m[a]==0){
-	    	continue;
-		}
 	    }
 	for(a=100;a<=999;a++){
 		if(m[a]==1){
@@ -81,13 +77,5 @@ class Superprime{
 	cout<<"超级素数的个数为"<<j<<endl;
 	cout<<"所有超级素数之和为"<<sum[2]<<endl;
 	cout<<"最大的超级素数为"<<max<<endl;
-		}
-	private:
-		int a,b=2,c[3]={0},m[1001]={0},n[3]={0},i=0,j=0,sum[3]={0},p=0,max=0;
-};
-int main()
-{
-	Superprime mine;
-	mine.shell();
 	return 0;
 }
