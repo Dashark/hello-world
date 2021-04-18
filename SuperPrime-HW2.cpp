738f0999dc9c2e6d44086461d51efd0f84e91974
firelinker746 <81558316+firelinker746@users.noreply.github.com>
2021-04-18 14:24:45 +0800

    Update SuperPrime-HW2.cpp

--- SuperPrime-HW2.cpp
+++ SuperPrime-HW2.cpp
@@ -1,57 +1,63 @@
-#include <iostream>
-#include <vector>
-class Nature {
-private:
-  int num;
-public:
-  Nature():num(0){
-    std::cout << "Default Create Nature as " << num << std::endl;
-  }
-  Nature(int n):num(n) {
-    std::cout << "Create Nature as " << num << std::endl;
-  }
-  Nature(const Nature &nat):num(nat.num){
-    std::cout << "Copy Create Nature as " << num << std::endl;
-  }
-  ~Nature() {
-    std::cout << "Destroy Nature as " << num << std::endl;
-  }
-};
-class SuperPrime {
-private:
-  std::vector<Nature> natures;
-public:
-  SuperPrime(int a, int b) {
-    std::cout << "Create SuperPrime from " << a << " to " << b << std::endl;
-    for(int i = a; i < b; i++) {
-      Nature nat(i);
-      std::cout << "HAHA" << std::endl;
-      natures.push_back(nat);
-      std::cout << "DDDDD" << std::endl;
+#include<iostream>
+#include<vector>
+using namespace std;
+int isPrime(int a)
+{
+	
+	int i,judge=1;
+	for(i=2;i<a;i++)
+	{
+		if(a%i==0)
+		judge=0;
 	}
-  }
-  ~SuperPrime() {
-    std::cout << "Destroy SuperPrime " << std::endl;
-  }
-  
-  Nature max() {
-  	std::vector<Nature>::iterate it = natures.begin();
-  	Nature max(0);
-  	for(; it != natures.end(); it ++) {
-  	  if(it->isSuperPrime()) {
-  	  	if (max.compare(*it)) {
-  	  	  max = *it;
+	return judge;
+}
+class SuperPrime
+{
+	private:
+	int w1,w2,w3,judge1,judge2,judge3,judge4;
+	public:
+		int count=0,total=0,max,a[900];
+		int isSuperPrime(int a)
+		{
+			judge1=0;judge2=0;judge3=0;judge4=0;
+		if(isPrime(a))
+		judge1=1;
+		w1=a%10;
+		w2=(a%100)/10;
+		w3=a/100;
+		if(isPrime(w1+w2+w3))
+		judge2=1;
+		if(isPrime(w1*w2*w3))
+		judge3=1;
+		if(isPrime(w1*w1+w2*w2+w3*w3))
+		judge4=1;
+		if(judge1&&judge2&&judge3&&judge4)
+		return 1;
+		else
+		return 0;
 		}
-	  }
+		void caozuo(int left,int right)
+		{
+			total=0;count=0;
+	for(int i=left;i<=right;i++)
+	{
+		
+		if(isSuperPrime(i))
+		{
+		a[count]=i;
+		total+=i;
+		count++;
+	    }
+	    max=a[count-1];
 	}
-  	return max;
-  }
+	}
 };
-int main() {
-  SuperPrime sp(10, 13);
-  Nature n = sp.max(); 
-  std::cout << "��󳬼�������" ;
-  n.show();
-  
-  return 0;
-} 
+int main()
+{
+	SuperPrime a;
+	a.caozuo(100,999);
+	cout<<endl;
+	cout<<"和："<<a.total<<endl<<"个数："<<a.count<<endl<<"最大："<<a.max;
+
+}
