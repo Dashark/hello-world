bf1b715abad3ab758cc8f07d4e9ca8a754213073
PeterZ Li <1846136440@qq.com>
2021-04-20 16:16:42 +0800

    try my best but fail

--- SuperPrime-HW2.cpp
+++ SuperPrime-HW2.cpp
@@ -5,37 +5,84 @@ private:
   int num;
 public:
   Nature():num(0){
-    std::cout << "Default Create Nature as " << num << std::endl;
   }
   Nature(int n):num(n) {
-    std::cout << "Create Nature as " << num << std::endl;
   }
   Nature(const Nature &nat):num(nat.num){
-    std::cout << "Copy Create Nature as " << num << std::endl;
   }
   ~Nature() {
-    std::cout << "Destroy Nature as " << num << std::endl;
+  }
+  bool isSuperPrime() const
+  {
+    for(int i=100;i<=999;i++)
+      {
+        int j;
+        for(j=2;j<i;j++)
+        {
+          if(i%j==0)
+            {
+              return false;
+            }
+        }
+        if(j==i)
+        {
+          int number[3],k=i;
+          for(int ni=0;ni<3;ni++)
+          {
+            number[ni]=k%10;
+            k=k/10;
+          }
+          int NumberSum=number[0]+number[1]+number[2];
+          for(int ni=2;ni<NumberSum;ni++)
+          {
+            if(NumberSum%ni==0)
+            {
+                return false;
+            }
+          }
+          int NumberMult=number[0]*number[1]*number[2];
+          for(int ni=2;ni<NumberMult;ni++)
+          {
+            if(NumberMult%ni==0)
+            {
+                return false;
+            }
+          }    
+          int NumberSqrt=number[0]*number[0]+number[1]*number[1]+number[2]*number[2];
+          for(int ni=2;ni<NumberSqrt;ni++)
+          {
+            if(NumberSqrt%ni==0)
+            {
+                return false;
+            }
+          }
+          return true;
+        }
+      }
+  }
+  bool compare(Nature p){
+    
+  }
+  void show(){
+
   }
 };
 class SuperPrime {
 private:
+  int LowLimit,HighLimit;
   std::vector<Nature> natures;
 public:
-  SuperPrime(int a, int b) {
-    std::cout << "Create SuperPrime from " << a << " to " << b << std::endl;
+  SuperPrime(int a, int b):LowLimit(a),HighLimit(b){
     for(int i = a; i < b; i++) {
       Nature nat(i);
-      std::cout << "HAHA" << std::endl;
       natures.push_back(nat);
-      std::cout << "DDDDD" << std::endl;
 	}
   }
   ~SuperPrime() {
-    std::cout << "Destroy SuperPrime " << std::endl;
   }
   
   Nature max() {
-  	std::vector<Nature>::iterate it = natures.begin();
+  	std::vector<Nature>::iterator it = natures.begin();
   	Nature max(0);
   	for(; it != natures.end(); it ++) {
   	  if(it->isSuperPrime()) {
@@ -48,10 +95,8 @@ public:
   }
 };
 int main() {
-  SuperPrime sp(10, 13);
+  SuperPrime sp(100,999);
   Nature n = sp.max(); 
-  std::cout << "��󳬼�������" ;
-  n.show();
-  
+  n.show();  
   return 0;
 } 
