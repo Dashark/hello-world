bb555cd11f5c25e278feba7cf23c57fcb5b09d41
maven <80609096+M-maven7@users.noreply.github.com>
2021-04-25 22:09:26 +0800

    Update SuperPrime-HW2.cpp

--- SuperPrime-HW2.cpp
+++ SuperPrime-HW2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include<cmath>
 class Nature {
 private:
   int num;
@@ -16,41 +17,87 @@ public:
   ~Nature() {
     std::cout << "Destroy Nature as " << num << std::endl;
   }
+  void show(){
+  	
+  }
+  bool isPrime() {
+    if(num == 1 || num == 0)
+    return false;
+    for(int i = 2; i <= (int)sqrt(num); i++)
+    {
+      if(num % i == 0)
+      return false;
+    }
+    return true;
+  }
+  int compare(const Nature &nat) {
+  	if (num > nat.num)
+  	  return 1;
+  	else if(num == nat.num)
+  	  return 0;
+  	
+  	return -1;
+  }
+private:
 };
-class SuperPrime {
+class SuperPrime : public Nature {
+private:
+  int num;
+public:
+  int max(){
+  	
+  }
+  SuperPrime(int n,int b) {
+  } 
+  SuperPrime(int n):num(n) {
+  } 
+  bool isSuperPrime() {
+  	Nature nat(num);
+  	return nat.isPrime();
+  } 
+  bool isPrime() {
+  	Nature nat(num);
+  	return nat.isPrime();
+  } 
+  int compare(const SuperPrime &nat) {
+  	if (num > nat.num)
+  	  return 1;
+  	else if(num == nat.num)
+  	  return 0;
+  	
+  	return -1;
+  }
+};
+class Container {
 private:
   std::vector<Nature> natures;
 public:
-  SuperPrime(int a, int b) {
+  Container(int a, int b) {
     std::cout << "Create SuperPrime from " << a << " to " << b << std::endl;
     for(int i = a; i < b; i++) {
-      Nature nat(i);
+      SuperPrime nat(i);
       std::cout << "HAHA" << std::endl;
-      natures.push_back(nat);
+      if(nat.isSuperPrime())
+        natures.push_back(nat);
       std::cout << "DDDDD" << std::endl;
 	}
   }
-  ~SuperPrime() {
-    std::cout << "Destroy SuperPrime " << std::endl;
-  }
   
-  Nature max() {
-  	std::vector<Nature>::iterate it = natures.begin();
+  SuperPrime max() {
+  	std::vector<Nature>::iterator it = natures.begin();
   	Nature max(0);
   	for(; it != natures.end(); it ++) {
-  	  if(it->isSuperPrime()) {
-  	  	if (max.compare(*it)) {
+      if (max.compare(*it)) {
   	  	  max = *it;
-		}
-	  }
+      }
 	}
-  	return max;
+  	return 0;
   }
 };
 int main() {
   SuperPrime sp(10, 13);
   Nature n = sp.max(); 
-  std::cout << "��󳬼�������" ;
+  std::cout << "・ｽ・ｽｬｼ・ｽ・ｽ・ｽ・ｽ・ｽ・ｽ・ｽ" ;
   n.show();
   
   return 0;
