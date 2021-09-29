01acbaf733172b041e815c738a2e33728db32872
Lake Hoo <hudasha@scu.edu.cn>
2021-09-29 15:26:00 +0800

    Update Course-3.cpp
    修改中文乱码提示

--- Course-3.cpp
+++ Course-3.cpp
@@ -7,15 +7,15 @@
 int main() {
   float x1, x2;
   float y, a, b, c;
-  printf("请输入系数：\n");
-  scanf("%f, %f, %f, %f", &y, &a, &b, &c);
+  printf("Please input \n");
+  scanf("%f %f %f %f", &y, &a, &b, &c);
   c =  c - y;
   a = 2 * a;
   float temp = sqrt(b*b - 4* a*c);
   x1 = (-b + temp)/(a);
   x2 = (-b - temp)/(a);
-  printf("一元二次方程根：\n");
-  printf("%f\n", x1);  //这是做什么？ 
+  printf("Results \n");
+  printf("%f\n", x1);  
   printf("%f\n", x2);
   return 0;
 } 
