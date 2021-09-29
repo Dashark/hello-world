ecc2342db1ff2cfbb2bdd7fea7dd5b4c5eed4109
Lake Hoo <hudasha@scu.edu.cn>
2021-09-29 15:21:13 +0800

    Update leapyear1.cpp
    重构版本

--- leapyear1.cpp
+++ leapyear1.cpp
@@ -4,8 +4,8 @@ int main()
   int year;
   scanf("%d", &year);
   if (year % 4 == 0)
-    if (year % 100 != 0)
-      printf("%d 年就是闰年\n", year);
+    if (year % 100 == 0)   //Refine  重构 
+      printf("%d 年不是闰年\n", year);
     else if (year % 400 == 0)
       printf("%d 年就是闰年\n", year);
     else
