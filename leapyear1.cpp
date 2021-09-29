ee0adb0d0cab2787c501577dc63808faf2ba3612
Lake Hoo <hudasha@scu.edu.cn>
2021-09-29 15:12:52 +0800

    Update leapyear1.cpp
    排版混乱

--- leapyear1.cpp
+++ leapyear1.cpp
@@ -3,15 +3,13 @@ int main() {
   int year;
   scanf("%d", &year);
   if (year % 4 == 0)
-    if(year % 100 != 0)
+  if (year % 100 != 0)
       printf("%d 年就是闰年\n", year); 
-    else // year % 4 == 0 && year % 100 == 0 确定 year % 400 != 0？ 、
-	  if(year % 400 == 0)
+ else if(year % 400 == 0)
 	    printf("%d 年就是闰年\n", year); 
-	  else
+  else
         printf("%d 年不是闰年\n", year);
-  else  //已知 year % 4 != 0  确定  year % 400 != 0
+  else  
     printf("%d 年不是闰年\n", year); 
-
   return 0;
 } 
