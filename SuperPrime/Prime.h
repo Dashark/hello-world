23ecb6786ec9bfa350bef9a00bbc4277f2ab5485
Dashark <dasha.hu@gmail.com>
2022-05-17 11:49:09 +0800

    接口变更

--- SuperPrime/Prime.h
+++ SuperPrime/Prime.h
@@ -6,7 +6,9 @@ using namespace std;
 class Prime{
 	public:
 		Prime(int low , int high);//构造函数 
-        static bool judgePrime(int);//判断一个数是否为素数 
+    Prime(int n);
+    bool judgePrime(int);//判断一个数是否为素数 
+    virtual bool isPrime();
 		void showPrime();//打印出所选范围内的所有素数，空格分隔 
 		int getSize();//获取素数个数 
 //		int *getPrime();//将vector中的素数放入一个数组并返回首地址，以供superPrime使用 
@@ -17,9 +19,10 @@ class Prime{
 class superPrime : public Prime {
 	public:
 		superPrime(int low , int high);//构造函数 
-		void showPrime();
+    superPrime(int n);
+    virtual bool isPrime();
 	private:
-        bool judgePrime(int);//判断是否为超级素数 
+    bool judgePrime(int);//判断是否为超级素数 
 		void findPrime(int , int);//找素数，放入vector 
 };
 
--- SuperPrime/main.cpp
+++ SuperPrime/main.cpp
@@ -1,19 +1,59 @@
 #include"Prime.cpp"
 
 template<class Type>
-void func(Type a) 
+void func(const Type &a) 
 {
 	//a.findPrime(100 , 999);
 	cout << "素数为：" << endl; 
 	a.showPrime();
 }
+
+void func(Prime *p)
+{
+  cout << "素数为：" << endl; 
+  p->showPrime();
+}
+void prime(int a, int b)
+{
+  std::vector<Prime*> ps;
+	for (int i = 100; i < 999; ++i) {
+		Prime * p = new Prime(i);
+		if (p->isPrime())
+			ps.push_back(p);
+    else
+      delete p;
+  }
+	for (Prime *p : ps) {
+		p->showPrime();
+		delete p;
+	}
+}
+void superprime(int a, int b)
+{
+  std::vector<Prime*> ps;
+	for (int i = 100; i < 999; ++i) {
+		Prime * p = new SuperPrime(i);
+		if (p->isPrime())
+			ps.push_back(p);
+    else
+      delete p;
+  }
+	for (Prime *p : ps) {
+		p->showPrime();
+		delete p;
+	}
+}  
 int main(){
-	
-	Prime a(100,999);  //强制的 
-	func(a);
-	
-	superPrime b(100,999);
-	func(b);
-	
+
+  Prime pm(100, 999);
+  func(pm);
+  func(&pm);
+
+  SuperPrime sp(100, 999);
+  func(sp);
+  func(&sp);
+
+  prime(100, 999);
+  superprime(100, 999);
 	return 0;
 }
