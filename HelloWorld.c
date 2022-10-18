7ca2fbbe5b24d7ed705615965a00c3614c3074f4
Lake Hu <hudasha@scu.edu.cn>
2022-10-18 14:25:27 +0800

    做了一个贾代马

--- HelloWorld.c
+++ HelloWorld.c
@@ -3,9 +3,6 @@ int main() {
   int x, y, temp;
   // 交换两个变量的值
   scanf("%d %d", &x, &y); 
-  temp = x;   // Copy 一份 
-  x = y;   // Copy y 
-  y = temp;   // 把x的拷贝赋值给y 
-  printf("x = %d, y = %d", x, y);
+  printf("x = %d, y = %d", y, x);
   return 0;
 }
