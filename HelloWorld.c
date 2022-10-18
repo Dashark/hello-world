3d790c0ba492126f8ef843a4f6bcca0724b20390
Lake Hu <hudasha@scu.edu.cn>
2022-10-18 14:16:43 +0800

    交换两个变量的值

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 int main() {
-  printf("Hello World!\n");
-  
-//try Jenkins again 
+  int x, y, temp;
+  // 交换两个变量的值
+  scanf("%d %d", &x, &y); 
+  temp = x;   // Copy 一份 
+  x = y;   // Copy y 
+  y = temp;   // 把x的拷贝赋值给y 
+  printf("x = %d, y = %d", x, y);
   return 0;
 }
