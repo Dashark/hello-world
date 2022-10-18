84ade2ab32b6ffd9a7bef59e293c646b7d3d5a2a
Lake Hu <hudasha@scu.edu.cn>
2022-10-18 14:58:09 +0800

    条件语句

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 int main() {
-  int x, y, temp;
-  // 交换两个变量的值
+  int x, y, z, temp;
+  // 输入两个变量，按照从大到小输出 
   scanf("%d %d", &x, &y); 
-  printf("x = %d, y = %d", y, x);
+  if ( x < y ) {
+    temp = x;   // Copy 一份 
+    x = y;   // Copy y 
+    y = temp;   // 把x的拷贝赋值给y 
+  }
+  printf("x = %d, y = %d", x, y);
   return 0;
 }
